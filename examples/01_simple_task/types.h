c1f14d75e3382d8374755442e4d8d9460d1eabc0
Ben Bergen <ben.bergen@gmail.com>
2015-11-04 21:44:24 -0800

    Tweaking implementation.

--- examples/01_simple_task/types.h
+++ examples/01_simple_task/types.h
@@ -1,5 +1,15 @@
 /*~--------------------------------------------------------------------------~*
- * placeholder
+ *  @@@@@@@@ @@       @@@@@@@@ @@     @@ @@
+ * /@@///// /@@      /@@///// //@@   @@ /@@
+ * /@@      /@@      /@@       //@@ @@  /@@
+ * /@@@@@@@ /@@      /@@@@@@@   //@@@   /@@
+ * /@@////  /@@      /@@////     @@/@@  /@@
+ * /@@      /@@      /@@        @@ //@@ /@@
+ * /@@      /@@@@@@@@/@@@@@@@@ @@   //@@/@@
+ * //       //////// //////// //     // // 
+ * 
+ * Copyright (c) 2016 Los Alamos National Laboratory, LLC
+ * All rights reserved
  *~--------------------------------------------------------------------------~*/
 
 #ifndef type_h
@@ -15,17 +25,15 @@ using cell_t = burton_mesh_t::cell_t;
 using point_t = burton_mesh_t::point_t;
 using vector_t = burton_mesh_t::vector_t;
 using real_t = burton_mesh_t::real_t;
+using accessor_t = burton_mesh_t::accessor_t<real_t>;
+using flexi::persistent;
 
 const size_t height(2);
 const size_t width(5);
 
-// FIXME: Need to expose this type in a simpler way...
-using accessor_t =
-  flexi::burton_mesh_traits_t::mesh_state_t::accessor_t<real_t>;
-using flexi::persistent;
-
 #endif // type_h
 
 /*~-------------------------------------------------------------------------~-*
+ * Formatting options
  * vim: set tabstop=2 shiftwidth=2 expandtab :
  *~-------------------------------------------------------------------------~-*/
