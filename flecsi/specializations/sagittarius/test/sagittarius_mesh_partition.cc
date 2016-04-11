fa1dd126e870136abdd083c60d1aca47efd6e254
Li-Ta Lo <lita.ollie@gmail.com>
2016-04-11 09:58:53 -0600

    add test on mesh_graph_partition::partition, which is the vtxdist in ParMetis, remove the dump test

--- flecsi/specializations/sagittarius/test/sagittarius_mesh_partition.cc
+++ flecsi/specializations/sagittarius/test/sagittarius_mesh_partition.cc
@@ -13,6 +13,7 @@
  *~-------------------------------------------------------------------------~~*/
 
 #include <cinchtest.h>
+#include <gmock/gmock.h>
 #include "../sagittarius_mesh.h"
 
 using namespace flecsi;
@@ -82,21 +83,22 @@ TEST_F(Sagittarius, number_partitions_should_be_2) {
 TEST_F(Sagittarius, number_of_vertices_in_each_partition_should_be_4) {
   ASSERT_EQ(4, partitions[0].offset.size());
   ASSERT_EQ(4, partitions[1].offset.size());
+
+  ASSERT_THAT(partitions[0].partition, ::testing::ElementsAre(0, 4, 4));
+  ASSERT_THAT(partitions[1].partition, ::testing::ElementsAre(0, 4, 4));
 }
 
-//TEST_F(Sagittarius, )
-TEST_F(Sagittarius, dump) {
-  for (auto partition : partitions) {
-    std::cout << "offset: ";
-    for (auto offset : partition.offset) {
-      std::cout << offset << " ";
-    }
-    std::cout << std::endl;
-    std::cout << "index: ";
-    for (auto i : partition.index) {
-      std::cout << i << " ";
-    }
-    std::cout << std::endl;
-  }
-  //ASSERT_EQ(0, partitions[1].partition.size());
+TEST_F(Sagittarius, degree_of_vertices) {
+  // FIXME: this test is still not intuitive enough.
+  auto diff0 = std::vector<size_t>(partitions[0].offset.size());
+  std::adjacent_difference(partitions[0].offset.begin(),
+                           partitions[0].offset.end(),
+                           diff0.begin());
+  ASSERT_THAT(diff0, ::testing::ElementsAre(0, 2, 3, 4));
+
+  auto diff1 = std::vector<size_t>(partitions[1].offset.size());
+  std::adjacent_difference(partitions[1].offset.begin(),
+                           partitions[1].offset.end(),
+                           diff1.begin());
+  ASSERT_THAT(diff1, ::testing::ElementsAre(0, 3, 2, 4));
 }
