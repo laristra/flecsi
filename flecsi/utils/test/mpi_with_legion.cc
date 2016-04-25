5fa5e633d0cc9a049424fa0d6f7b663b4b255da5
Irina <irina@f23vm.localdomain>
2016-04-25 16:42:12 -0600

    Added "hello world" task to test how legion works inside MPI program

--- flecsi/utils/test/mpi_with_legion.cc
+++ flecsi/utils/test/mpi_with_legion.cc
@@ -29,6 +29,7 @@ using return_type_t = execution_t::return_type_t;
 enum TaskIDs{
  TOP_LEVEL_TASK_ID,
  CONNECT_MPI_TASK_ID,
+ HELLOWORLD_TASK_ID,
 };
 
 ExtLegionHandshake *handshake;
@@ -65,8 +66,13 @@ void top_level_task(const Task *task,
                                        Domain::from_rect<2>(all_procs),
                                        TaskArgument(0, 0),
                                        arg_map);
+  IndexLauncher helloworld_launcher(HELLOWORLD_TASK_ID,
+                               Domain::from_rect<2>(all_procs),
+                               TaskArgument(0, 0),
+                               arg_map);
   //run legion_init() from each thead
   runtime->execute_index_space(ctx, connect_mpi_launcher);
+  runtime->execute_index_space(ctx, helloworld_launcher);
 
   printf("connect_mpi finished \n");
 
@@ -82,6 +88,14 @@ void connect_mpi_task (const Task *task,
   printf ("inside connect_mpi_task \n");
 #endif
     handshake->legion_init();
+}
+
+
+void helloworld_mpi_task (const Task *task,
+                      const std::vector<PhysicalRegion> &regions,
+                      Context ctx, HighLevelRuntime *runtime)
+{
+  printf ("helloworld \n");
 }
 
 void complete_legion_configure(void)
@@ -103,13 +117,20 @@ void run_legion_task(void)
 
 void my_init_legion(){
 
+  handshake = new ExtLegionHandshake(ExtLegionHandshake::IN_EXT, 1, 1);
+
   HighLevelRuntime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
+
   HighLevelRuntime::register_legion_task<top_level_task>( TOP_LEVEL_TASK_ID,
                           Processor::LOC_PROC, true/*single*/, false/*index*/, 
                           AUTO_GENERATE_ID, TaskConfigOptions(), "top_level_task");
   HighLevelRuntime::register_legion_task< connect_mpi_task>( CONNECT_MPI_TASK_ID, 
                           Processor::LOC_PROC, false/*single*/, true/*index*/,
                           AUTO_GENERATE_ID, TaskConfigOptions(true/*leaf*/), "connect_mpi_task");
+  
+  HighLevelRuntime::register_legion_task< helloworld_mpi_task >( HELLOWORLD_TASK_ID,
+                          Processor::LOC_PROC, false/*single*/, true/*index*/,
+                          AUTO_GENERATE_ID, TaskConfigOptions(true/*leaf*/), "hellowrld_task");
 
 
   const InputArgs &args = HighLevelRuntime::get_input_args();
@@ -129,7 +150,7 @@ void my_init_legion(){
 TEST(mpi_with_legion, simple) {
    ASSERT_LT(execute(world_size), 1);
 
-   handshake = new ExtLegionHandshake(ExtLegionHandshake::IN_EXT, 1, 1);
+//   handshake = new ExtLegionHandshake(ExtLegionHandshake::IN_EXT, 1, 1);
    my_init_legion(); 
  
 } // TEST
