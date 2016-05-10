16e412abeb172a16d7d769ba93ebb0c2f735236f
Christoph Junghans <junghans@lanl.gov>
2016-05-10 14:51:11 -0600

    execution/test/legion.cc: fixed a warning

--- flecsi/execution/test/legion.cc
+++ flecsi/execution/test/legion.cc
@@ -63,7 +63,8 @@ TEST(legion, register_task) {
 //} // TEST
 
 TEST(driver, execute) {
-	char* argv = "shit";
+	char dummy[] = "shit";
+	char *argv = &(dummy[0]);
 	flecsi::execution_t<flecsi::legion_execution_policy_t>::execute_driver(flecsi::top_level_task,1,&argv);
 } // TEST
 
