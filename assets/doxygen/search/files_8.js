d4f6fefa58141c145ca3906ea3ccfc49750e46e0
Automatic Deployment (Travis CI) <flecsi-commit@lanl.gov>
2016-08-12 23:39:16 +0000

    Documentation Update

--- assets/doxygen/search/files_8.js
+++ assets/doxygen/search/files_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['register_5flegion_2eh',['register_legion.h',['../register__legion_8h.html',1,'']]]
+  ['register_5flegion_2eh',['register_legion.h',['../register__legion_8h.html',1,'']]],
+  ['reorder_2eh',['reorder.h',['../reorder_8h.html',1,'']]]
 ];
