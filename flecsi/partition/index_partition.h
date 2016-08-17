e02d6c7cf667782a12b95c9b00aed09085310251
Ben Bergen <bergen@lanl.gov>
2016-08-17 11:52:49 -0600

    Beginning to work on index partitions for mesh.

---
+++ flecsi/partition/index_partition.h
@@ -0,0 +1,40 @@
+/*~--------------------------------------------------------------------------~*
+ * Copyright (c) 2015 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~--------------------------------------------------------------------------~*/
+
+#ifndef flecsi_dmp_index_partition_h
+#define flecsi_dmp_index_partition_h
+
+/*!
+ * \file index_partition.h
+ * \authors bergen
+ * \date Initial file creation: Aug 17, 2016
+ */
+
+namespace flecsi {
+namespace dmp {
+
+/*!
+  \class partition__ index_partition.h
+  \brief partition__ provides...
+ */
+template<typename T>
+struct index_partition__
+{
+  using identifier_t = T;
+
+  std::vector<identifier_t> local;
+  std::vector<identifier_t> shared;
+  std::vector<identifier_t> ghost;
+}; // class partition__
+
+} // namespace dmp
+} // namespace flecsi
+
+#endif // flecsi_dmp_index_partition_h
+
+/*~-------------------------------------------------------------------------~-*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~-------------------------------------------------------------------------~-*/
---
+++ flecsi/partition/test/index_partition.cc
@@ -0,0 +1,113 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~-------------------------------------------------------------------------~~*/
+
+#include <cinchtest.h>
+#include <cmath>
+#include <cstdlib>
+
+#include "flecsi/partition/index_partition.h"
+
+using index_partition_t = flecsi::dmp::index_partition__<size_t>;
+
+// NxN block mesh
+static constexpr size_t N = 8;
+static constexpr size_t ranks = 2;
+
+TEST(index_partition, basic) {
+
+  index_partition_t ip;
+  auto parts = std::ldiv(N, ranks);
+
+  for(size_t j(0); j<N; ++j) {
+    for(size_t i(0); i<N; ++i) {
+      size_t id = i + j*N;
+
+      if(i<(parts.quot-1)) {
+        ip.local.push_back(id);
+      }
+      else if(i<parts.quot) {
+        ip.shared.push_back(id);
+      }
+      else if(i<parts.quot+1) {
+        ip.ghost.push_back(id);
+      } // if
+    } // for
+  } // for
+
+  std::cout << "local: ";
+  for(auto l: ip.local) {
+    std::cout << l << " ";
+  } // for
+  std::cout << std::endl;
+
+  std::cout << "shared: ";
+  for(auto l: ip.shared) {
+    std::cout << l << " ";
+  } // for
+  std::cout << std::endl;
+
+  std::cout << "ghost: ";
+  for(auto l: ip.ghost) {
+    std::cout << l << " ";
+  } // for
+  std::cout << std::endl;
+
+} // TEST
+
+/*----------------------------------------------------------------------------*
+ * Cinch test Macros
+ *
+ *  ==== I/O ====
+ *  CINCH_CAPTURE()              : Insertion stream for capturing output.
+ *                                 Captured output can be written or
+ *                                 compared using the macros below.
+ *
+ *    EXAMPLE:
+ *      CINCH_CAPTURE() << "My value equals: " << myvalue << std::endl;
+ *
+ *  CINCH_COMPARE_BLESSED(file); : Compare captured output with
+ *                                 contents of a blessed file.
+ *
+ *  CINCH_WRITE(file);           : Write captured output to file.
+ *
+ *  CINCH_ASSERT(ASSERTION, ...) : Call Google test macro and automatically
+ *                                 dump captured output (from CINCH_CAPTURE)
+ *                                 on failure.
+ *
+ *  CINCH_EXPECT(ASSERTION, ...) : Call Google test macro and automatically
+ *                                 dump captured output (from CINCH_CAPTURE)
+ *                                 on failure.
+ *
+ * Google Test Macros
+ *
+ * Basic Assertions:
+ *
+ *  ==== Fatal ====             ==== Non-Fatal ====
+ *  ASSERT_TRUE(condition);     EXPECT_TRUE(condition)
+ *  ASSERT_FALSE(condition);    EXPECT_FALSE(condition)
+ *
+ * Binary Comparison:
+ *
+ *  ==== Fatal ====             ==== Non-Fatal ====
+ *  ASSERT_EQ(val1, val2);      EXPECT_EQ(val1, val2)
+ *  ASSERT_NE(val1, val2);      EXPECT_NE(val1, val2)
+ *  ASSERT_LT(val1, val2);      EXPECT_LT(val1, val2)
+ *  ASSERT_LE(val1, val2);      EXPECT_LE(val1, val2)
+ *  ASSERT_GT(val1, val2);      EXPECT_GT(val1, val2)
+ *  ASSERT_GE(val1, val2);      EXPECT_GE(val1, val2)
+ *
+ * String Comparison:
+ *
+ *  ==== Fatal ====                     ==== Non-Fatal ====
+ *  ASSERT_STREQ(expected, actual);     EXPECT_STREQ(expected, actual)
+ *  ASSERT_STRNE(expected, actual);     EXPECT_STRNE(expected, actual)
+ *  ASSERT_STRCASEEQ(expected, actual); EXPECT_STRCASEEQ(expected, actual)
+ *  ASSERT_STRCASENE(expected, actual); EXPECT_STRCASENE(expected, actual)
+ *----------------------------------------------------------------------------*/
+
+/*~------------------------------------------------------------------------~--*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~------------------------------------------------------------------------~--*/
