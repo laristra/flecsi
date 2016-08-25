649c21731a7350ee80707a16b075458af2d080a6
Ben Bergen <bergen@lanl.gov>
2016-08-25 12:14:01 -0600

    Fix for MPI builds.
    New app example needs MPI runtime initialization.

--- bin/example_app.cc
+++ bin/example_app.cc
@@ -3,6 +3,11 @@
  * All rights reserved.
  *~-------------------------------------------------------------------------~~*/
 
+#if defined(FLECSI_RUNTIME_MODEL_mpilegion) || \
+  defined(FLECSI_RUNTIME_MODEL_mpi)
+  #include <mpi.h>
+#endif
+
 #include "flecsi/execution/execution.h"
 
 ///
@@ -12,7 +17,21 @@
 ///
 
 int main(int argc, char ** argv) {
-  return flecsi::execution::context_t::instance().initialize(argc, argv);
+
+#if defined(FLECSI_RUNTIME_MODEL_mpilegion) || \
+  defined(FLECSI_RUNTIME_MODEL_mpi)
+  MPI_Init(&argc, &argv);
+#endif
+
+  // Call FleCSI runtime initialization
+  auto retval = flecsi::execution::context_t::instance().initialize(argc, argv);
+
+#if defined(FLECSI_RUNTIME_MODEL_mpilegion) || \
+  defined(FLECSI_RUNTIME_MODEL_mpi)
+  MPI_Finalize();
+#endif
+
+  return retval;
 } // main
 
 /*~------------------------------------------------------------------------~--*
