92fcc75350004f44b02552ebce1304a0fa826aa1
nickm319 <nickm@lanl.gov>
2016-10-05 13:28:24 -0600

    Add index space unit test.

---
+++ flecsi/topology/test/index-space.cc
@@ -0,0 +1,54 @@
+#include <cinchtest.h>
+#include <iostream>
+
+#include "flecsi/topology/index_space.h"
+
+using namespace std;
+using namespace flecsi;
+using namespace topology;
+
+struct object_id{
+  size_t id;
+
+  object_id(size_t id)
+  : id(id){}
+
+  operator size_t(){
+    return id;
+  }
+
+  size_t index_space_index() const{
+    return id;
+  }
+
+  bool operator<(const object_id& oid) const{
+    return id < oid.id;
+  }
+};
+
+struct object{
+  object(object_id id)
+  : id(id){}
+
+  using id_t = object_id;
+
+  object_id index_space_id() const{
+    return id;
+  }
+
+  object_id id;
+};
+
+TEST(index_space, index_space) {
+  using index_space_t = index_space<object*, true, true, false>;
+
+  index_space_t is;
+
+  for(size_t i = 0; i < 10000; ++i){
+    is << new object(i);
+  }
+
+  for(auto oi : is){
+    cout << oi->id << endl;
+  }
+}
