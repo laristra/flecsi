826ff9af34905d063194d3391ad7fba1b3ddea31
Ben Bergen <bergen@lanl.gov>
2016-11-21 19:19:20 -0700

    Consolidation...

--- flecsi/io/set_utils.h
+++ flecsi/io/set_utils.h
@@ -19,6 +19,38 @@
 namespace flecsi {
 namespace io {
 
+std::set<size_t>
+set_union(
+  std::set<size_t> s1,
+  std::set<size_t> s2
+)
+{
+  std::vector<size_t> uvec;
+
+  std::set_union(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    std::back_inserter(uvec));
+
+  std::set<size_t> sunion(uvec.begin(), uvec.end());
+
+  return sunion;
+} // set_union
+
+std::set<size_t>
+set_difference(
+  std::set<size_t> s1,
+  std::set<size_t> s2
+)
+{
+  std::vector<size_t> vdiff;
+
+  std::set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    std::inserter(vdiff, vdiff.begin()));
+
+  std::set<size_t> difference(vdiff.begin(), vdiff.end());
+
+  return difference;
+} // set_difference
+
 ///
 // Return the neighboring cells for the given cell id.
 //
@@ -68,7 +100,32 @@ cell_neighbors(
   std::set<size_t> neighbors(nvec.begin(), nvec.end());
 
   return neighbors;
-} // nearest_neighbors
+} // cell_neighbors
+
+///
+// Return the dependency closure of the given set.
+//
+// \param md The mesh_definition_t object to query for mesh information.
+// \param indices The cell indeces of the initial set.
+///
+std::set<size_t>
+cell_closure(
+  mesh_definition_t & md,
+  std::set<size_t> indices
+)
+{
+  std::vector<size_t> cvec;
+  std::set<size_t> closure;
+
+  // Iterate over the cell indices and add all neighbors
+  for(auto i: indices) {
+    auto ncurr = cell_neighbors(md, i);
+
+    closure = flecsi::io::set_union(ncurr, closure);
+  } // for
+
+  return closure;
+} // cell_closure
 
 } // namespace io
 } // namespace flecsi
