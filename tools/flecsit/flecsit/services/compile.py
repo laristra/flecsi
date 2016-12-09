5afadd3f7e06a777729be376fdedf67ac33c5296
Ben Bergen <bergen@lanl.gov>
2016-12-08 20:12:31 -0700

    Modifications to flecsit program
    - Also updated some documentation and remoded charter

--- tools/flecsit/flecsit/services/compile.py
+++ tools/flecsit/flecsit/services/compile.py
@@ -36,9 +36,32 @@ class FleCSIT_Analysis(Service):
                  ' multiple times. Arguments may be of the form' +
                  ' -I/path/to/include, -I /path/to/include, or' +
                  ' --include /path/to/include. Include paths may' +
-                 ' also be specified by setting the FLECSIT_INCLUDE_PATH' +
-                 ' environment variable. If FLECSIT_INCLUDE_PATH is set,' +
+                 ' also be specified by setting the FLECSIT_INCLUDES' +
+                 ' environment variable. If FLECSIT_INCLUDES is set,' +
                  ' it will override any include paths passed as' +
+                 ' command line arguements.'
+        )
+
+        self.parser.add_argument('-L', '--ldflags', action='append',
+            help='Specify a linker path. This argument may be given' +
+                 ' multiple times. Arguments may be of the form' +
+                 ' -L/path/to/link, -L /path/to/link, or' +
+                 ' --ldflags /path/to/include. Linker paths may' +
+                 ' also be specified by setting the FLECSIT_LDFLAGS' +
+                 ' environment variable. If FLECSIT_LDFLAGS is set,' +
+                 ' it will override any ldflags passed as' +
+                 ' command line arguements.'
+        )
+
+        self.parser.add_argument('-l', '--library', action='append',
+            help='Specify a link library. This argument may be given' +
+                 ' multiple times. Arguments may be of the form' +
+                 ' -lname, -l name, --library name, or' +
+                 ' -l/full/path/libname.{a,so}, -l /full/path/libname.{a,so},' +
+                 ' , or --library /full/path/libname.{a,so}. Libraries may' +
+                 ' also be specified by setting the FLECSIT_LIBRARIES' +
+                 ' environment variable. If FLECSIT_LIBRARIES is set,' +
+                 ' it will override any libraries passed as' +
                  ' command line arguements.'
         )
 
@@ -68,11 +91,14 @@ class FleCSIT_Analysis(Service):
         # Process command-line arguments
         #----------------------------------------------------------------------#
 
-        # Add any user-provided include paths to build
-        env_include_path = os.getenv('FLECSIT_INCLUDE_PATH')
+        # Read environment variables
+        env_includes = os.getenv('FLECSIT_INCLUDES')
+        env_ldflags = os.getenv('FLECSIT_LDFLAGS')
+        env_libraries = os.getenv('FLECSIT_LIBRARIES')
 
-        if env_include_path is not None:
-            for include in env_include_path.split(':') or []:
+        # Add any user-provided include paths to build
+        if env_includes is not None:
+            for include in env_includes.split(':') or []:
                 build['includes'] += ' -I' + include
         else:
             for include in args.include or []:
@@ -83,6 +109,22 @@ class FleCSIT_Analysis(Service):
 
         # Add current directory to includes
         build['includes'] += ' -I./'
+
+        # Add any user-provided ldflags to build
+        if env_ldflags is not None:
+            for path in env_ldflags.split(':') or []:
+                build['libraries'] += ' -L' + path
+        else:
+            for path in args.ldflags or []:
+                build['libraries'] += ' -L' + path
+
+        # Add any user-provided libraries to build
+        if env_libraries is not None:
+            for lib in env_libraries.split(':') or []:
+                build['libraries'] += ' -l' + lib
+        else:
+            for lib in args.library or []:
+                build['libraries'] += ' -l' + lib
 
         # Add driver to build defines
         build['defines'] += ' -DFLECSI_DRIVER=' + args.driver
