2c096fb75cd0902b50b46fe663c9531ccdf054b9
Ben Bergen <bergen@lanl.gov>
2016-12-19 16:06:27 -0700

    Working on cleanup.

--- flecsi/io/io.h
+++ flecsi/io/io.h
@@ -12,29 +12,30 @@
  * All rights reserved
  *~--------------------------------------------------------------------------~*/
 
-#ifndef flecsi_io_h
-#define flecsi_io_h
+#ifndef flecsi_io_io_h
+#define flecsi_io_io_h
 
 #include "flecsi/io/io_base.h"
 
-/*!
- * \file io.h
- * \authors wohlbier
- * \date Initial file creation: Oct 07, 2015
- */
+///
+/// \file
+/// \date Initial file creation: Oct 07, 2015
+///
 
-namespace flecsi
-{
-/*!
-  \brief Generic mesh reader that calls the correct method based on the suffix.
+namespace flecsi {
+namespace io {
 
-  \tparam mesh_t Mesh type to read.
-
-  \param[in] name Read mesh \e m from \e name.
-  \param[out] m Create mesh \e m from \e name.
-
-  \return Error code. 0 on success.
- */
+///
+/// \brief Generic mesh reader that calls the correct method based on
+///        the suffix.
+///
+/// \tparam mesh_t Mesh type to read.
+///
+/// \param[in] name Read mesh \e m from \e name.
+/// \param[out] m Create mesh \e m from \e name.
+///
+/// \return Error code. 0 on success.
+///
 template <typename mesh_t>
 int32_t read_mesh(const std::string & name, mesh_t & m)
 {
@@ -49,20 +50,21 @@ int32_t read_mesh(const std::string & name, mesh_t & m)
   // clean up and return
   delete io;
   return ret;
-}
+} // read_mesh
 
-/*!
-  \brief Generic mesh writer that calls the correct method based on the suffix.
-
-  \tparam mesh_t Mesh type to write.
-
-  \param[in] name Write mesh \e m to \e name.
-  \param[in] m Write mesh \e m to \e name.
-
-  \return Error code. 0 on success.
- */
-// FIXME: should allow for const mesh_t &
-// int32_t write_mesh(const std::string &name, const mesh_t &m) {
+///
+/// \brief Generic mesh writer that calls the correct method based
+///        on the suffix.
+///
+/// \tparam mesh_t Mesh type to write.
+///
+/// \param[in] name Write mesh \e m to \e name.
+/// \param[in] m Write mesh \e m to \e name.
+///
+/// \return Error code. 0 on success.
+///
+/// \note should allow for const mesh_t &
+/// int32_t write_mesh(const std::string &name, const mesh_t &m)
 template <typename mesh_t>
 int32_t write_mesh(const std::string & name, mesh_t & m)
 {
@@ -77,21 +79,23 @@ int32_t write_mesh(const std::string & name, mesh_t & m)
   // clean up and return
   delete io;
   return ret;
-}
+} // write_mesh
 
-/*!
-  \brief Generic mesh writer that calls the correct method based on the suffix.
-
-  \tparam mesh_t Mesh type to write.
-
-  \param[in] name Write mesh \e m to \e name.
-  \param[in] m Write mesh \e m to \e name.
-  \param[in] m A binary flag.
-
-  \return Error code. 0 on success.
- */
-// FIXME: should allow for const mesh_t &
-// int32_t write_mesh(const std::string &name, const mesh_t &m) {
+///
+/// \brief Generic mesh writer that calls the correct method based
+///        on the suffix.
+///
+/// \tparam mesh_t Mesh type to write.
+///
+/// \param[in] name Write mesh \e m to \e name.
+/// \param[in] m Write mesh \e m to \e name.
+/// \param[in] m A binary flag.
+///
+/// \return Error code. 0 on success.
+///
+/// \note should allow for const mesh_t &
+/// int32_t write_mesh(const std::string &name, const mesh_t &m)
+///
 template <typename mesh_t>
 int32_t write_mesh(const std::string & name, mesh_t & m, bool binary)
 {
@@ -106,13 +110,12 @@ int32_t write_mesh(const std::string & name, mesh_t & m, bool binary)
   // clean up and return
   delete io;
   return ret;
-}
+} // write_mesh
 
-
-
+} // namespace io
 } // namespace flecsi
 
-#endif // flecsi_io_h
+#endif // flecsi_io_io_h
 
 /*~-------------------------------------------------------------------------~-*
  * Formatting options
