92f2ebf66c1681c4444c45923884712a5832a8d3
Ben Bergen <bergen@lanl.gov>
2017-01-11 19:26:51 -0700

    Adding reflection files.

---
+++ flecsi/utils/test/reflection.cc
@@ -0,0 +1,26 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~-------------------------------------------------------------------------~~*/
+
+#include <cinchdevel.h>
+
+#if 0
+#include "flecsi/utils/reflection.h"
+
+struct test_type_t
+{
+  declare_reflected (
+    (double) r1,
+    (int) r2
+  )
+}; // struct test_type_t
+#endif
+
+DEVEL(reflection) {
+} // DEVEL
+
+/*~------------------------------------------------------------------------~--*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~------------------------------------------------------------------------~--*/
