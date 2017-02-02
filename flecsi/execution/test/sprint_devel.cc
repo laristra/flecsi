9d055511daa9480be83ea04f0204e6350f1f854f
Irina Demeshko <irina@lanl.gov>
2017-02-02 09:27:55 -0700

    Chanding status of sprint test to the "DEVEL"  test

---
+++ flecsi/execution/test/sprint_devel.cc
@@ -0,0 +1,30 @@
+/*~-------------------------------------------------------------------------~~*
+ *  @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+ * /@@/////  /@@          @@////@@ @@////// /@@
+ * /@@       /@@  @@@@@  @@    // /@@       /@@
+ * /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+ * /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+ * /@@       /@@/@@//// //@@    @@       /@@/@@
+ * /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+ * //       ///  //////   //////  ////////  // 
+ * 
+ * Copyright (c) 2016 Los Alamos National Laboratory, LLC
+ * All rights reserved
+ *~-------------------------------------------------------------------------~~*/
+
+#include <cinchdevel.h>
+
+#include "flecsi/execution/context.h"
+
+DEVEL(sprint) {
+
+  // This translation unit doesn't do anything, but it is currently
+  // required to complete compilation. The actual test logic is
+  // in 'task_driver.h'.
+
+} // TEST
+
+/*~------------------------------------------------------------------------~--*
+ * Formatting options
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~------------------------------------------------------------------------~--*/
