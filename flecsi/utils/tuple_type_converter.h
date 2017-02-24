77afcf07603c98a0b06748b9101f33be1af704fb
Nick Moss <nickm@lanl.gov>
2017-02-24 09:58:34 -0700

    Add tuple converter with base class checker.

--- flecsi/utils/tuple_type_converter.h
+++ flecsi/utils/tuple_type_converter.h
@@ -17,7 +17,7 @@
 namespace flecsi {
 namespace utils {
 
-  template<class T>
+  template<typename T>
   struct convert_tuple_type_{
     using type = T;
   };
@@ -25,9 +25,31 @@ namespace utils {
   template<typename... Args>
   struct convert_tuple_type;
 
-  template <typename... Args>
+  template<typename... Args>
   struct convert_tuple_type<std::tuple<Args...>>{
     using type = std::tuple<typename convert_tuple_type_<Args>::type...>;
+  };
+
+  template<typename T, typename TO, bool B>
+  struct base_convert_tuple_type_;
+
+  template<typename T, typename TO>
+  struct base_convert_tuple_type_<T, TO, true>{
+    using type = TO;
+  };
+
+  template<typename T, typename TO>
+  struct base_convert_tuple_type_<T, TO, false>{
+    using type = T;
+  };
+
+  template<typename... Args>
+  struct base_convert_tuple_type;
+
+  template<typename B, typename TO, typename... Args>
+  struct base_convert_tuple_type<B, TO, std::tuple<Args...>>{
+    using type = std::tuple<typename base_convert_tuple_type_<Args, TO, 
+      std::is_base_of<B, Args>::value>::type...>;
   };
 
 } // namespace utils
