f762cae91f7f1888dc4c35e56ed5c4ef42702fd1
Galen M Shipman <gshipman@lanl.gov>
2017-03-09 15:19:25 -0500

    modify test_mpi_legion_interop to allow passing command line params through to the legion runtime. Added singleton to wrap mpi_legion_interop_t as we don't use the ctx singleton here and need to ensure we have only one mpi_legion_interop_t around.

--- flecsi/execution/test/mpilegion/test_mpi_legion_interop.cc
+++ flecsi/execution/test/mpilegion/test_mpi_legion_interop.cc
@@ -19,20 +19,9 @@
 #include <type_traits> // std::is_same
 
 // user includes
-
-#include "flecsi/execution/execution.h"
-#include "flecsi/execution/task.h"
-#include "flecsi/utils/any.h"
-#include "flecsi/partition/index_partition.h"
+#include "flecsi/execution/test/mpilegion/interop_init.h"
 
 using namespace flecsi::execution;
-/// Task IDs
-enum TaskIDs{
- TOP_LEVEL_TASK_ID         =0x00000010,
- HELLOWORLD_TASK_ID        =0x00000100,
- SPMD_INIT_TID             =0x00000200,
-};
-
 
 
 //make Array global only for the simple test example
@@ -44,9 +33,7 @@ const int nElements=10;
 using namespace LegionRuntime::HighLevel;
 using namespace LegionRuntime::Accessor;
 using namespace LegionRuntime::Arrays;
-using index_partition_t = flecsi::dmp::index_partition__<size_t>;
 
-static mpi_legion_interop_t InteropHelper;
 
 /* ------------------------------------------------------------------------ */
  ///
@@ -56,16 +43,16 @@ static mpi_legion_interop_t InteropHelper;
                     const std::vector<PhysicalRegion> &regions,
                     Context ctx, Runtime *runtime)
  {
-  InteropHelper.connect_with_mpi(ctx, runtime);
+  InteropS::getInstance().helper.connect_with_mpi(ctx, runtime);
 
   std::cout<<"inside TLT:after connect_with_mpi"<<std::endl;
 
-  InteropHelper.wait_on_mpi(ctx, runtime);
+  InteropS::getInstance().helper.wait_on_mpi(ctx, runtime);
   ArgumentMap arg_map;
   IndexLauncher helloworld_launcher(
        HELLOWORLD_TASK_ID,
        Domain::from_rect<1>(
-              InteropHelper.all_processes_),
+              InteropS::getInstance().helper.all_processes_),
        TaskArgument(0, 0),
        arg_map);
 
@@ -74,20 +61,20 @@ static mpi_legion_interop_t InteropHelper;
   fm2.wait_all_results();
 
   //handoff to MPI
-  InteropHelper.handoff_to_mpi(ctx, runtime);
+  InteropS::getInstance().helper.handoff_to_mpi(ctx, runtime);
 
-  InteropHelper.wait_on_mpi(ctx, runtime);
+  InteropS::getInstance().helper.wait_on_mpi(ctx, runtime);
 
   std::cout<< "back to TLT after MPI" <<std::endl;
 
   //check array storage 
 
-   index_partition_t ip = InteropHelper.data_storage_[0];
-   double A= InteropHelper.data_storage_[1];
+   index_partition_t ip = InteropS::getInstance().helper.data_storage_[0];
+   double A= InteropS::getInstance().helper.data_storage_[1];
    std::cout<<"storage[1] = " << A <<std::endl;
    assert (A==3.14);
  
-  InteropHelper.handoff_to_mpi(ctx, runtime);
+  InteropS::getInstance().helper.handoff_to_mpi(ctx, runtime);
 
    
  }
@@ -108,53 +95,56 @@ void helloworld_mpi_task (const Task *legiontask,
 ///
 /// legion initialization function
 ///
-void my_init_legion(){
+// void my_init_legion(){
   
-  InteropHelper.legion_configure();
+//   InteropHelper.legion_configure();
 
-  HighLevelRuntime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
-  HighLevelRuntime::register_legion_task<top_level_task>(TOP_LEVEL_TASK_ID,
-      Processor::LOC_PROC, true/*single*/, false/*index*/);
+//   HighLevelRuntime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
+//   HighLevelRuntime::register_legion_task<top_level_task>(TOP_LEVEL_TASK_ID,
+//       Processor::LOC_PROC, true/*single*/, false/*index*/);
 
-  HighLevelRuntime::register_legion_task< helloworld_mpi_task >(
-        HELLOWORLD_TASK_ID,
-        Processor::LOC_PROC,
-        false/*single*/, true/*index*/,
-        AUTO_GENERATE_ID, 
-        TaskConfigOptions(true/*leaf*/),
-        "hellowrld_task");
+//   HighLevelRuntime::register_legion_task< helloworld_mpi_task >(
+//         HELLOWORLD_TASK_ID,
+//         Processor::LOC_PROC,
+//         false/*single*/, true/*index*/,
+//         AUTO_GENERATE_ID, 
+//         TaskConfigOptions(true/*leaf*/),
+//         "hellowrld_task");
 
-     InteropHelper.register_tasks();
-     InteropHelper.initialize();
+//      InteropHelper.register_tasks();
+//      InteropHelper.initialize();
 
-  char arguments[] = "1";
-  char * argv = &arguments[0];
-  HighLevelRuntime::start(1, &argv, true);
+//   char arguments[] = "1";
+//   char * argv = &arguments[0];
+//   HighLevelRuntime::start(1, &argv, true);
 
 
-  InteropHelper.handoff_to_legion();
+//   InteropHelper.handoff_to_legion();
 
-  InteropHelper.wait_on_legion();
+//   InteropHelper.wait_on_legion();
 
-  //check data_storage_
-  index_partition_t ip;
-  double A=3.14;
-  InteropHelper.data_storage_.push_back(flecsi::utils::any_t(ip));
-  InteropHelper.data_storage_.push_back(flecsi::utils::any_t(A));
+//   //check data_storage_
+//   index_partition_t ip;
+//   double A=3.14;
+//   InteropHelper.data_storage_.push_back(flecsi::utils::any_t(ip));
+//   InteropHelper.data_storage_.push_back(flecsi::utils::any_t(A));
 
-  InteropHelper.handoff_to_legion();
+//   InteropHelper.handoff_to_legion();
  
-  InteropHelper.wait_on_legion();
-  Legion::Runtime::wait_for_shutdown();
-  std::cout<<"back to MPI to finalize"<<std::endl;
-}
+//   InteropHelper.wait_on_legion();
+//   Legion::Runtime::wait_for_shutdown();
+//   std::cout<<"back to MPI to finalize"<<std::endl;
+// }
 
 /* ------------------------------------------------------------------------ */
 ///
 /// test body
 ///
+#define execute(task, ...) \
+  execution_t::execute_task(task, ##__VA_ARGS__)
+
 TEST(mpi_legion_interop, simple) {
-   my_init_legion(); 
+  //my_init_legion(); 
  
 } // TEST
 
