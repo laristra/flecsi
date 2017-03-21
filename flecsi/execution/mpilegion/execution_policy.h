0e9c9300a14a3f31b2eb0324a9a0097840f8b716
nickm319 <nickm@lanl.gov>
2017-03-21 09:47:16 -0600

    Fix problem with RR's.

--- flecsi/execution/mpilegion/execution_policy.h
+++ flecsi/execution/mpilegion/execution_policy.h
@@ -58,19 +58,19 @@ namespace execution {
       flecsi::execution::field_ids_t & fid_t = 
         flecsi::execution::field_ids_t::instance();
 
-      RegionRequirement rr1(h.exclusive_lr, EP, EXCLUSIVE, h.exclusive_lr);
+      RegionRequirement rr1(h.exclusive_lr, Legion::PrivilegeMode(EP), EXCLUSIVE, h.exclusive_lr);
       rr1.add_field(fid_t.fid_value);
-      // JPG - does not work l.add_region_requirement(rr1);
+      l.add_region_requirement(rr1);
       h.exclusive_priv = EP;
 
-      RegionRequirement rr2(h.shared_lr, SP, EXCLUSIVE, h.shared_lr);
+      RegionRequirement rr2(h.shared_lr, Legion::PrivilegeMode(SP), EXCLUSIVE, h.shared_lr);
       rr2.add_field(fid_t.fid_value);
-      // JPG - does not work l.add_region_requirement(rr2);
+      l.add_region_requirement(rr2);
       h.shared_priv = SP;
 
-      RegionRequirement rr3(h.ghost_lr, GP, EXCLUSIVE, h.ghost_lr);
+      RegionRequirement rr3(h.ghost_lr, Legion::PrivilegeMode(GP), EXCLUSIVE, h.ghost_lr);
       rr3.add_field(fid_t.fid_value);
-      // JPG - does not work l.add_region_requirement(rr3);
+      l.add_region_requirement(rr3);
       h.ghost_priv = GP;
     }
 
