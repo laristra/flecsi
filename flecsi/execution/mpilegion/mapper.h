239121a6e82b416fb406927c92597c25a3345bac
Galen M Shipman <gshipman@lanl.gov>
2017-03-27 17:41:09 -0400

    cleanup mapper

--- flecsi/execution/mpilegion/mapper.h
+++ flecsi/execution/mpilegion/mapper.h
@@ -79,15 +79,15 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
   , machine(machine)
 
   {
-  	using legion_machine=LegionRuntime::HighLevel::Machine;
-   	using legion_proc=LegionRuntime::HighLevel::Processor;
-   
-   	legion_machine::ProcessorQuery pq = 
-           legion_machine::ProcessorQuery(machine).same_address_space_as(local);   
-		for(legion_machine::ProcessorQuery::iterator pqi = pq.begin();
+    using legion_machine=LegionRuntime::HighLevel::Machine;
+    using legion_proc=LegionRuntime::HighLevel::Processor;
+    
+    legion_machine::ProcessorQuery pq = 
+        legion_machine::ProcessorQuery(machine).same_address_space_as(local);   
+    for(legion_machine::ProcessorQuery::iterator pqi = pq.begin();
         pqi != pq.end();
         ++pqi)
-   	{   
+    {   
       legion_proc p = *pqi;
       if(p.kind() == legion_proc::LOC_PROC)
       	local_cpus.push_back(p);
@@ -99,29 +99,29 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
       std::map<Realm::Memory::Kind, Realm::Memory>& mem_map = proc_mem_map[p];
       
       legion_machine::MemoryQuery mq = 
-            legion_machine::MemoryQuery(machine).has_affinity_to(p);
+          legion_machine::MemoryQuery(machine).has_affinity_to(p);
       for(legion_machine::MemoryQuery::iterator mqi = mq.begin();
-         mqi != mq.end();
-         ++mqi)
+          mqi != mq.end();
+          ++mqi)
       {           
       	Realm::Memory m = *mqi;
         mem_map[m.kind()] = m;
-                  
+        
         if(m.kind() == Realm::Memory::SYSTEM_MEM)
-        	local_sysmem = m;
+          local_sysmem = m;
       }//end for
-   	}//end for
-   
-   	std::cout << "Mapper constuctor: local=" << local << " cpus=" <<
-         local_cpus.size() << " gpus=" << local_gpus.size() <<
-          " sysmem=" << local_sysmem<<std::endl;
+    }//end for
+    
+    std::cout << "Mapper constuctor: local=" << local << " cpus=" <<
+        local_cpus.size() << " gpus=" << local_gpus.size() <<
+        " sysmem=" << local_sysmem<<std::endl;
   }// end mpi_mapper_t
-
+  
   ///
   /// Destructor
   ///
   virtual ~mpi_mapper_t(){};
-
+  
   ///
   ///  The slice_task call is used by the runtime
   ///  to query the mapper about the best way to distribute
@@ -136,13 +136,13 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
   virtual
   void
   slice_task(
-  	const Legion::Mapping::MapperContext ctx,
-    const Legion::Task& task,
-    const Legion::Mapping::Mapper::SliceTaskInput& input,
-    Legion::Mapping::Mapper::SliceTaskOutput& output
-  )
+      const Legion::Mapping::MapperContext ctx,
+      const Legion::Task& task,
+      const Legion::Mapping::Mapper::SliceTaskInput& input,
+      Legion::Mapping::Mapper::SliceTaskOutput& output
+             )
   {
-  	using legion_proc=LegionRuntime::HighLevel::Processor;
+    using legion_proc=LegionRuntime::HighLevel::Processor;
     size_t connect_mpi_task_id = task_ids_t::instance().connect_mpi_task_id;
     size_t handoff_to_mpi_task_id =
         task_ids_t::instance().handoff_to_mpi_task_id;
@@ -151,37 +151,37 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
         task_ids_t::instance().update_mappers_task_id;
 
 
- 		// tag-based decisions here
-  	if(task.task_id == connect_mpi_task_id||
-      task.task_id == handoff_to_mpi_task_id||
-      task.task_id == wait_on_mpi_task_id ||
-      (task.tag & MAPPER_FORCE_RANK_MATCH) != 0) {
-    	// expect a 1-D index domain
-    	assert(input.domain.get_dim() == 1);
-    	LegionRuntime::Arrays::Rect<1> r = input.domain.get_rect<1>();
-
-    	// each point in the domain goes to CPUs assigned to that rank
-    	output.slices.resize(r.volume());
-    	size_t idx = 0;
-     
-			using legion_machine=LegionRuntime::HighLevel::Machine;
+    // tag-based decisions here
+    if(task.task_id == connect_mpi_task_id||
+       task.task_id == handoff_to_mpi_task_id||
+       task.task_id == wait_on_mpi_task_id ||
+       (task.tag & MAPPER_FORCE_RANK_MATCH) != 0) {
+      // expect a 1-D index domain
+      assert(input.domain.get_dim() == 1);
+      LegionRuntime::Arrays::Rect<1> r = input.domain.get_rect<1>();
+      
+      // each point in the domain goes to CPUs assigned to that rank
+      output.slices.resize(r.volume());
+      size_t idx = 0;
+      
+      using legion_machine=LegionRuntime::HighLevel::Machine;
       using legion_proc=LegionRuntime::HighLevel::Processor;
-
+      
       // get list of all processors and make sure the count matches
       legion_machine::ProcessorQuery pq =
-        legion_machine::ProcessorQuery(machine).only_kind(
-                            legion_proc::LOC_PROC);
+          legion_machine::ProcessorQuery(machine).only_kind(
+              legion_proc::LOC_PROC);
       std::vector<legion_proc> all_procs(pq.begin(), pq.end());
       assert((r.lo[0] == 0) && (r.hi[0] == (int)(all_procs.size() - 1)));
-
-    	for(LegionRuntime::Arrays::GenericPointInRectIterator<1> pir(r);
-         pir; ++pir, ++idx) {
+      
+      for(LegionRuntime::Arrays::GenericPointInRectIterator<1> pir(r);
+          pir; ++pir, ++idx) {
       	output.slices[idx].domain =Legion::Domain::from_rect<1>(
-              LegionRuntime::Arrays::Rect<1>(pir.p, pir.p));
-        	output.slices[idx].proc = all_procs[idx];
-   	  }//end for
-    	return;
-		}//endi if MAPPER_FORCE_RANK_MATCH
+            LegionRuntime::Arrays::Rect<1>(pir.p, pir.p));
+        output.slices[idx].proc = all_procs[idx];
+      }//end for
+      return;
+    }//endi if MAPPER_FORCE_RANK_MATCH
 
 /*  	if((task.tag & MAPPER_SUBRANK_LAUNCH) != 0) {
     	// expect a 1-D index domain
@@ -196,51 +196,51 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
 */
 
   	// task-specific cases below
-      if((task.tag & MAPPER_ALL_PROC) != 0)
-		{
-    	// expect a 2-D index domain - first component is the processor index,
-    	//  and second is the MPI rank
-    	assert(input.domain.get_dim() == 2);
-    	LegionRuntime::Arrays::Rect<2> r = input.domain.get_rect<2>();
-
-    	using legion_machine=LegionRuntime::HighLevel::Machine;
-    	using legion_proc=LegionRuntime::HighLevel::Processor;
-
-     	// get list of all processors and make sure the count matches
-    	legion_machine::ProcessorQuery pq =
-        legion_machine::ProcessorQuery(machine).only_kind(
-                            legion_proc::LOC_PROC);
-    	std::vector<legion_proc> all_procs(pq.begin(), pq.end());
-    	assert((r.lo[0] == 0) && (r.hi[0] == (int)(all_procs.size() - 1)));
-
-    	// now create a slice for each processor, allowing it to try to connect to
-    	//  each MPI rank
-    	output.slices.resize(all_procs.size());
-    	for(size_t i = 0; i < all_procs.size(); i++) {
+    if((task.tag & MAPPER_ALL_PROC) != 0)
+    {
+      // expect a 2-D index domain - first component is the processor index,
+      //  and second is the MPI rank
+      assert(input.domain.get_dim() == 2);
+      LegionRuntime::Arrays::Rect<2> r = input.domain.get_rect<2>();
+      
+      using legion_machine=LegionRuntime::HighLevel::Machine;
+      using legion_proc=LegionRuntime::HighLevel::Processor;
+      
+      // get list of all processors and make sure the count matches
+      legion_machine::ProcessorQuery pq =
+          legion_machine::ProcessorQuery(machine).only_kind(
+              legion_proc::LOC_PROC);
+      std::vector<legion_proc> all_procs(pq.begin(), pq.end());
+      assert((r.lo[0] == 0) && (r.hi[0] == (int)(all_procs.size() - 1)));
+      
+      // now create a slice for each processor, allowing it to try to connect to
+      //  each MPI rank
+      output.slices.resize(all_procs.size());
+      for(size_t i = 0; i < all_procs.size(); i++) {
       	LegionRuntime::Arrays::Rect<2> subrect = r;
       	subrect.lo.x[0] = subrect.hi.x[0] = i;
       	output.slices[i].domain = Legion::Domain::from_rect<2>(subrect);
       	output.slices[i].proc = all_procs[i];
-    	}//end for
-    	return;
-  	} //end if task.task_id=
-
-//TOFIX
-/*
-  	else if( task.task_id ==update_mappers_task_id)
-		{
-    	// expect a 1-D index domain - each point goes to the corresponding node
-    	assert(input.domain.get_dim() == 1);
-    	Rect<1> r = input.domain.get_rect<1>();
-
-    	// go through all the CPU processors and find a representative for each
-    	//  node (i.e. address space)
-    	std::map<int, legion_proc> targets;
-
-    	using legion_machine=LegionRuntime::HighLevel::Machine;
-    	legion_machine::ProcessorQuery pq =
-      	legion_machine::ProcessorQuery(machine).only_kind(
-																					legion_proc::LOC_PROC);
+      }//end for
+      return;
+    } //end if task.task_id=
+    
+    //TOFIX
+    /*
+      else if( task.task_id ==update_mappers_task_id)
+      {
+      // expect a 1-D index domain - each point goes to the corresponding node
+      assert(input.domain.get_dim() == 1);
+      Rect<1> r = input.domain.get_rect<1>();
+      
+      // go through all the CPU processors and find a representative for each
+      //  node (i.e. address space)
+      std::map<int, legion_proc> targets;
+      
+      using legion_machine=LegionRuntime::HighLevel::Machine;
+      legion_machine::ProcessorQuery pq =
+      legion_machine::ProcessorQuery(machine).only_kind(
+      legion_proc::LOC_PROC);
 
     	for(legion_machine::ProcessorQuery::iterator it = pq.begin();
       	it != pq.end(); ++it)
@@ -260,19 +260,19 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
     	return;
   	} //end else if task.task_id=
 */
-   	else{
-			DefaultMapper::default_slice_task(task, local_cpus, remote_cpus,
-                               input, output, cpu_slices_cache);
-   	}//end else
+    else{
+      DefaultMapper::default_slice_task(task, local_cpus, remote_cpus,
+                                        input, output, cpu_slices_cache);
+    }//end else
   } //end slice_task
-                         
-  protected:
-
+  
+ protected:
+  
   std::map<LegionRuntime::Arrays::Point<1>,
-      std::vector<LegionRuntime::HighLevel::Processor>, 
-      STLComparator<1> > rank_cpus, rank_gpus;
+           std::vector<LegionRuntime::HighLevel::Processor>, 
+           STLComparator<1> > rank_cpus, rank_gpus;
   std::map<LegionRuntime::HighLevel::Processor,
-       std::map<Realm::Memory::Kind, Realm::Memory> > proc_mem_map;
+           std::map<Realm::Memory::Kind, Realm::Memory> > proc_mem_map;
   Realm::Memory local_sysmem;
   Realm::Machine machine;
 };
@@ -284,14 +284,14 @@ mpi_mapper_t : public Legion::Mapping::DefaultMapper
 inline
 void
 mapper_registration(
-   LegionRuntime::HighLevel::Machine machine,
-   LegionRuntime::HighLevel::HighLevelRuntime *rt,
-   const std::set<LegionRuntime::HighLevel::Processor> &local_procs
-)
+    LegionRuntime::HighLevel::Machine machine,
+    LegionRuntime::HighLevel::HighLevelRuntime *rt,
+    const std::set<LegionRuntime::HighLevel::Processor> &local_procs
+                    )
 {
   for (std::set<LegionRuntime::HighLevel::Processor>::const_iterator
-        it = local_procs.begin();
-        it != local_procs.end(); it++)
+           it = local_procs.begin();
+       it != local_procs.end(); it++)
   {
     mpi_mapper_t *mapper = new mpi_mapper_t(machine, rt, *it);
     rt->replace_default_mapper(mapper, *it);
