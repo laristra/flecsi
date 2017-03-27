41edb937bc80c2d4e6d1b3e9a6d15fa2ecfc735a
Jonathan Pietarila Graham <jpietarilagraham@gmail.com>
2017-03-27 09:35:27 -0600

    mpilegion_ghost ghost access test passes

--- flecsi/execution/test/check_ghost_access.h
+++ flecsi/execution/test/check_ghost_access.h
@@ -48,17 +48,11 @@ shared_write_task(
 {
   std::cout << my_color << " as master writes data; phase 1 of cycle " << cycle << std::endl;
 
-  for (size_t i = 0; i < acc_cells.shared_size(); i++) {
+  for (size_t i = 0; i < acc_cells.shared_size(); i++)
     acc_cells.shared(i) = static_cast<size_t>(global_IDs.shared(i) + cycle);
-    std::cout << my_color << " wrote "
-        << static_cast<size_t>(global_IDs.shared(i) + cycle) << " to " << i << std::endl;
-  }
 
-  for (size_t i = 0; i < acc_cells.shared_size(); i++) {
+  for (size_t i = 0; i < acc_cells.shared_size(); i++)
     acc_cells.shared(i) = static_cast<size_t>(global_IDs.shared(i) + cycle);
-    std::cout << my_color << " read "
-        << acc_cells.shared(i) << " from " << i << std::endl;
-  }
 }
 
 flecsi_register_task(shared_write_task, loc, single);
@@ -74,16 +68,13 @@ ghost_read_task(
 {
   std::cout << my_color << " as slave reads data; phase 2 of cycle " << cycle <<  std::endl;
 
-/*  for (size_t i = 0; i < acc_cells.ghost_size(); i++) {
-    std::cout << acc_cells.ghost(i) << " vs "
-        << static_cast<size_t>(global_IDs.ghost(i) + cycle) << std::endl;
-//    assert(acc_cells.ghost(i) == static_cast<size_t>(global_IDs.ghost(i) + cycle));
-  }
-*/
+  // If shared data was lost ghost will not be available
   for (size_t i = 0; i < acc_cells.shared_size(); i++)
-    std::cout << my_color << " REread "
-        << acc_cells.shared(i) << " from " << i << std::endl;
-//    assert(acc_cells.shared(i) == static_cast<size_t>(global_IDs.shared(i) + cycle));
+    assert(acc_cells.shared(i) == static_cast<size_t>(global_IDs.shared(i) + cycle));
+
+  for (size_t i = 0; i < acc_cells.ghost_size(); i++)
+    assert(acc_cells.ghost(i) == static_cast<size_t>(global_IDs.ghost(i) + cycle));
+
 }
 
 flecsi_register_task(ghost_read_task, loc, single);
@@ -98,7 +89,6 @@ driver(
   flecsi::execution::context_t & context_ = flecsi::execution::context_t::instance();
   const LegionRuntime::HighLevel::Task *task = context_.task(flecsi::utils::const_string_t{"driver"}.hash());
   const int my_color = task->index_point.point_data[0];
-	std::cout << my_color << " check GHOST" << std::endl;
 
 	flecsi::data_client& dc = *((flecsi::data_client*)argv[argc - 1]);
 
@@ -110,19 +100,20 @@ driver(
   auto cell_ID_handle =
     flecsi_get_handle(dc, sprint, cell_ID, size_t, dense, index_space, none, ro, ro);
 
-  for (int cycle = 0; cycle < 1; cycle++) {
+  for (int cycle = 0; cycle < 3; cycle++) {
 
     // phase WRITE: masters update their halo regions; slaves may not access data
 
-    std::cout << my_color << " write phase " << cycle << std::endl;
     flecsi_execute_task(shared_write_task, loc, single, cell_ID_handle, shared_write_handle, my_color, cycle);
 
     // phase READ: slaves can read data; masters may not write to data
 
-    std::cout << my_color << " read phase " << cycle << std::endl;
     flecsi_execute_task(ghost_read_task, loc, single, cell_ID_handle, ghost_read_handle, my_color, cycle);
 
   }
+
+  std::cout << "flecsi test ghost access ... passed"
+  << std::endl;
 
 } //driver
 
