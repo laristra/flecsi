fecb2e64f36c6699726437e6ce7ff18ab65e4f4f
Ben Bergen <bergen@lanl.gov>
2017-04-01 23:28:58 -0600

    Added unit test for pure legion registration.

---
+++ flecsi/execution/test/legion/pure.cc
@@ -0,0 +1,19 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *~-------------------------------------------------------------------------~~*/
+
+#include "flecsi/execution/legion/pure.h"
+
+///
+/// \file
+/// \date Initial file creation: Apr 01, 2017
+///
+
+int driver(int argc, char ** argv) {
+} // driver
+
+/*~------------------------------------------------------------------------~--*
+ * Formatting options for vim.
+ * vim: set tabstop=2 shiftwidth=2 expandtab :
+ *~------------------------------------------------------------------------~--*/
