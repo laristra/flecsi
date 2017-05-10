1a647a16857ac663b65647d6c4e7b3fa01816ed0
Nick Moss <nickm@lanl.gov>
2017-05-10 08:59:46 -0600

    Remove duplicate legion_runtime_state.

--- flecsi/execution/legion/context_policy.h
+++ flecsi/execution/legion/context_policy.h
@@ -49,36 +49,6 @@ namespace flecsi {
 namespace execution {
 
 //----------------------------------------------------------------------------//
-//! The legion_runtime_state_t type provides storage for Legion runtime
-//! information that can be reinitialized as needed to store const
-//! data types and references as required by the Legion runtime.
-//!
-//! @ingroup legion-execution
-//----------------------------------------------------------------------------//
-
-struct legion_runtime_state_t {
-
-  legion_runtime_state_t(
-    Legion::Context & context_,
-    Legion::HighLevelRuntime * runtime_,
-    const Legion::Task * task_,
-    const std::vector<Legion::PhysicalRegion> & regions_
-  )
-  :
-    context(context_),
-    runtime(runtime_),
-    task(task_),
-    regions(regions_)
-  {}
-
-  Legion::Context & context;
-  Legion::HighLevelRuntime * runtime;
-  const Legion::Task * task;
-  const std::vector<Legion::PhysicalRegion> & regions;
-
-}; // struct legion_runtime_state_t
-
-//----------------------------------------------------------------------------//
 //! Context state uses thread-local storage (TLS). The state is set for
 //! each Legion task invocation using the task name hash of the plain-text
 //! task name as a key. This should give sufficient isolation from naming
