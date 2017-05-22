7259ac95da1eb7fde9750cbe523108a3f6fc49ae
nickm319@github.com <nickm@lanl.gov>
2017-05-22 16:25:53 -0600

    Add internal fid's, use internal fid for ghost owner.

--- flecsi/execution/legion/runtime_driver.cc
+++ flecsi/execution/legion/runtime_driver.cc
@@ -16,6 +16,7 @@
 #include "flecsi/execution/context.h"
 #include "flecsi/execution/legion/legion_tasks.h"
 #include "flecsi/execution/legion/mapper.h"
+#include "flecsi/execution/legion/internal_field.h"
 #include "flecsi/utils/common.h"
 
 #include <legion_utilities.h>
@@ -100,6 +101,9 @@ runtime_driver(
 
   auto coloring_info = context_.coloring_info_map();
 
+  auto ghost_owner_fid = 
+    LegionRuntime::HighLevel::FieldID(internal_field::ghost_owner);
+
   {
   clog_tag_guard(runtime_driver);
 
@@ -134,7 +138,8 @@ runtime_driver(
       runtime->create_field_allocator(ctx, expanded_fs);
 
     // temporary fix until we add registration of internal fields
-    allocator.allocate_field(sizeof(LegionRuntime::Arrays::Point<2>), 42);
+    allocator.allocate_field(sizeof(LegionRuntime::Arrays::Point<2>), 
+      ghost_owner_fid);
 
     // Get field info for this index space
     auto fitr = context_.field_info_map().find(handle_idx.first);
@@ -196,7 +201,7 @@ runtime_driver(
     compaction_launcher.add_region_requirement(
         Legion::RegionRequirement(color_lp, 0/*projection ID*/,
             WRITE_DISCARD, EXCLUSIVE, expanded_lregions_map[handle]))
-                .add_field(42);  // FIXME register in some way not magic number
+                .add_field(ghost_owner_fid);  // FIXME register in some way not magic number
   } // for handle
   runtime->execute_index_space(ctx, compaction_launcher);
 
@@ -255,7 +260,7 @@ runtime_driver(
 
       Legion::RegionRequirement rr(color_lr, READ_WRITE, SIMULTANEOUS, expanded_lregions_map[handle]);
 
-      rr.add_field(42);  // FIXME need to do user fields, reference field, connectivity field
+      rr.add_field(ghost_owner_fid);  // FIXME need to do user fields, reference field, connectivity field
       
       auto fitr = context_.field_info_map().find(handle);
       
@@ -286,7 +291,7 @@ runtime_driver(
         spmd_launcher.add_region_requirement(
           Legion::RegionRequirement(ghost_owner_lr, READ_ONLY, SIMULTANEOUS, expanded_lregions_map[handle])
           .add_flags(NO_ACCESS_FLAG)
-          .add_field(42));  // FIXME need to do user fields, reference field, connectivity field
+          .add_field(ghost_owner_fid));  // FIXME need to do user fields, reference field, connectivity field
 
       } // for owner
 
