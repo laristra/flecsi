de54fc4576d4e3c7b3f35196d34c8eb338ea249c
nickm319@github.com <nickm@lanl.gov>
2017-06-12 16:46:07 -0600

    Potential bug fix.

--- flecsi/data/legion/data_handle_policy.h
+++ flecsi/data/legion/data_handle_policy.h
@@ -57,7 +57,7 @@ struct legion_data_handle_policy_t
   Legion::LogicalRegion ghost_lr;
   Legion::PhaseBarrier* pbarrier_as_owner_ptr;
   std::vector<Legion::PhaseBarrier*>* ghost_owners_pbarriers_ptrs;
-  std::vector<Legion::LogicalRegion> ghost_owners_lregions;
+  std::vector<Legion::LogicalRegion>* ghost_owners_lregions;
   Legion::STL::map<LegionRuntime::Arrays::coord_t,
     LegionRuntime::Arrays::coord_t>* global_to_local_color_map;
   Legion::LogicalRegion color_region;
