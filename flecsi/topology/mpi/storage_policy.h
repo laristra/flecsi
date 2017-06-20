35885960122e2de56f0b0625c184bcb34c38f80c
nickm319@github.com <nickm@lanl.gov>
2017-06-20 16:41:08 -0600

    Mesh topology calls mesh storage to add entities.

--- flecsi/topology/mpi/storage_policy.h
+++ flecsi/topology/mpi/storage_policy.h
@@ -33,16 +33,38 @@ namespace topology {
 /// \brief mpi_data_handle_policy_t provides...
 ///
 
-template <size_t D, size_t NM>
+template <size_t ND, size_t NM>
 struct mpi_topology_storage_policy_t
 {
+  using id_t = utils::id_t;
+
   using index_spaces_t = 
-    std::array<index_space<mesh_entity_base_*, true, true, true>, D + 1>;
+    std::array<index_space<mesh_entity_base_*, true, true, true>, ND + 1>;
 
   // array of array of domain_connectivity
-  std::array<std::array<domain_connectivity<D>, NM>, NM> topology;
+  std::array<std::array<domain_connectivity<ND>, NM>, NM> topology;
 
   std::array<index_spaces_t, NM> index_spaces;
+
+  template<size_t D, size_t M, typename ET>
+  void
+  add_entity(
+    ET* ent,
+    size_t partition_id = 0
+  )
+  {
+    using dtype = domain_entity<M, ET>;
+
+    auto & is = index_spaces[M][ND].template cast<dtype>();
+
+    id_t global_id = id_t::make<ND, M>(is.size(), partition_id);
+
+    auto typed_ent = static_cast<mesh_entity_base_t<NM>*>(ent);
+
+    typed_ent->template set_global_id<M>(global_id);
+    is.push_back(ent);
+  }
+  
 }; // class mpi_topology_storage_policy_t
 
 } // namespace topology
