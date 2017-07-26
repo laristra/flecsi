b1408f6e91e939851b083190292bab68770f858e
Ben Bergen <bergen@lanl.gov>
2017-07-26 14:18:47 -0600

    Intermediate check-in.

--- flecsi/execution/test/data_client_handle.cc
+++ flecsi/execution/test/data_client_handle.cc
@@ -234,7 +234,10 @@ namespace execution {
 
 void specialization_tlt_init(int argc, char ** argv) {
   clog(info) << "In specialization top-level-task init" << std::endl;
-  flecsi_execute_mpi_task(add_colorings, 0);
+  coloring_map_t map;
+  map.vertices = 1;
+  map.cells = 0;
+  flecsi_execute_mpi_task(add_colorings, map);
 
   auto& context = execution::context_t::instance();
 
