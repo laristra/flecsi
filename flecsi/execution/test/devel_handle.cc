0fcb2cb17fa0c8c03d30334dfb55e03318defdfb
Ben Bergen <bergen@lanl.gov>
2017-08-07 13:06:57 -0600

    Cleanup of test.

--- flecsi/execution/test/devel_handle.cc
+++ flecsi/execution/test/devel_handle.cc
@@ -59,44 +59,45 @@ void initialize_mesh(mesh<wo> m) {
   clog(info) << "initialize_mesh task" << std::endl;
   } // scope
 
-  auto & context = execution::context_t::instance();
+  auto & context { execution::context_t::instance() };
 
-  auto & vertex_map = context.index_map(index_spaces::vertices);
-  auto & reverse_vertex_map = context.reverse_index_map(index_spaces::vertices);
-  auto & cell_map = context.index_map(index_spaces::cells);
+  auto & vertex_map { context.index_map(index_spaces::vertices) };
+  auto & reverse_vertex_map
+    { context.reverse_index_map(index_spaces::vertices) };
+  auto & cell_map { context.index_map(index_spaces::cells) };
 
   std::vector<vertex_t *> vertices;
 
-  const size_t width = 8;
-  const double dt = 1.0/width;
+  const size_t width { 8 };
+  const double dt { 1.0/width };
 
   for(auto & vm: vertex_map) {
-    const size_t mid = vm.second;
-    const size_t row = mid/(width+1);
-    const size_t column = mid%(width+1);
+    const size_t mid { vm.second };
+    const size_t row { mid/(width+1) };
+    const size_t column { mid%(width+1) };
 
-    point_t p{column*dt, row*dt};
+    point_t p { column*dt, row*dt };
     vertices.push_back(m.make<vertex_t>(p));
   } // for
 
   size_t count{0};
   for(auto & cm: cell_map) {
-    const size_t mid = cm.second;
+    const size_t mid { cm.second };
 
-    const size_t row = mid/width;
-    const size_t column = mid%width;
+    const size_t row { mid/width };
+    const size_t column { mid%width };
 
-    const size_t v0 = (column    ) + (row    ) * (width + 1);
-    const size_t v1 = (column + 1) + (row    ) * (width + 1);
-    const size_t v2 = (column + 1) + (row + 1) * (width + 1);
-    const size_t v3 = (column    ) + (row + 1) * (width + 1);
+    const size_t v0 { (column    ) + (row    ) * (width + 1) };
+    const size_t v1 { (column + 1) + (row    ) * (width + 1) };
+    const size_t v2 { (column + 1) + (row + 1) * (width + 1) };
+    const size_t v3 { (column    ) + (row + 1) * (width + 1) };
 
-    const size_t lv0 = reverse_vertex_map[v0];
-    const size_t lv1 = reverse_vertex_map[v1];
-    const size_t lv2 = reverse_vertex_map[v2];
-    const size_t lv3 = reverse_vertex_map[v3];
+    const size_t lv0 { reverse_vertex_map[v0] };
+    const size_t lv1 { reverse_vertex_map[v1] };
+    const size_t lv2 { reverse_vertex_map[v2] };
+    const size_t lv3 { reverse_vertex_map[v3] };
 
-    auto c = m.make<cell_t>();
+    auto c { m.make<cell_t>() };
     m.init_cell<0>(c, { vertices[lv0], vertices[lv1],
       vertices[lv2], vertices[lv3] });
   } // for
@@ -112,9 +113,9 @@ flecsi_register_task(initialize_mesh, loc, single);
 //----------------------------------------------------------------------------//
 
 void initialize_pressure(mesh<ro> m, field<rw, rw, ro> p) {
-  size_t count(0);
+  size_t count{0};
 
-  auto & context = execution::context_t::instance();
+  auto & context { execution::context_t::instance() };
 
   for(auto c: m.cells(owned)) {
     p(c) = (context.color() + 1)*1000.0 + count++;
@@ -129,7 +130,7 @@ flecsi_register_task(initialize_pressure, loc, single);
 //----------------------------------------------------------------------------//
 
 void update_pressure(mesh<ro> m, field<rw, rw, ro> p) {
-  size_t count(0);
+  size_t count{0};
 
   for(auto c: m.cells(owned)) {
     p(c) = 2.0*p(c);
@@ -196,16 +197,14 @@ void specialization_tlt_init(int argc, char ** argv) {
   clog(info) << "specialization_tlt_init function" << std::endl;
   } // scope
 
-  coloring_map_t map;
-  map.vertices = index_spaces::vertices;
-  map.cells = index_spaces::cells;
+  coloring_map_t map { index_spaces::vertices, index_spaces::cells };
 
   flecsi_execute_mpi_task(add_colorings, map);
 
-  auto & context = execution::context_t::instance();
+  auto & context { execution::context_t::instance() };
 
-  auto & vinfo = context.coloring_info(index_spaces::vertices);
-  auto & cinfo = context.coloring_info(index_spaces::cells);
+  auto & vinfo { context.coloring_info(index_spaces::vertices) };
+  auto & cinfo { context.coloring_info(index_spaces::cells) };
 
   adjacency_info_t ai;
   ai.index_space = index_spaces::cells_to_vertices;
@@ -214,7 +213,7 @@ void specialization_tlt_init(int argc, char ** argv) {
   ai.color_sizes.resize(cinfo.size());
 
   for(auto & itr : cinfo){
-    size_t color = itr.first;
+    size_t color{itr.first};
     const coloring_info_t & ci = itr.second;
     ai.color_sizes[color] = (ci.exclusive + ci.shared + ci.ghost) * 4;
   } // for
