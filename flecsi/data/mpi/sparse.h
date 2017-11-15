b009c9b6a23cd98244ad28e0010bcec6f6d7fc91
Nick Moss <nickm@lanl.gov>
2017-11-15 08:41:08 -0700

    Remove accessor-specific methods from handles

--- flecsi/data/mpi/sparse.h
+++ flecsi/data/mpi/sparse.h
@@ -86,28 +86,6 @@ struct sparse_handle_t : public sparse_data_handle__<T, EP, SP, GP>
   // Constructors.
   //--------------------------------------------------------------------------//
 
-  // FIXME: calling to base class constructor?
-  ///
-  /// Default constructor.
-  ///
-  sparse_handle_t() {}
-
-	///
-  /// Copy constructor.
-	///
-//	sparse_handle_t(
-//    const sparse_handle_t & a
-//  )
-//  :
-//    label_(a.label_)
-//  {}
-
-  template<size_t EP2, size_t SP2, size_t GP2>
-  sparse_handle_t(const sparse_handle_t<T, EP2, SP2, GP2> & h)
-    : base(reinterpret_cast<const base&>(h)),
-      label_(h.label_)
-  {}
-
   sparse_handle_t(
     size_t num_exclusive,
     size_t num_shared,
@@ -117,9 +95,6 @@ struct sparse_handle_t : public sparse_data_handle__<T, EP, SP, GP>
 
   template<typename, size_t, size_t, size_t>
   friend class sparse_handle_t;
-
-private:
-  std::string label_ = "";
 }; // struct sparse_handle_t
 
 //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=//
