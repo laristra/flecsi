9f0224bfcfb1fb010fc11191a35176975b0a2562
Nick Moss <nickm@lanl.gov>
2018-01-05 14:46:10 -0800

    changes to set topology handle to allow for dependent or independent set topology

--- flecsi/data/set_topology_handle.h
+++ flecsi/data/set_topology_handle.h
@@ -28,19 +28,43 @@ enum class set_topology_buffer_t
 template<
   typename DATA_CLIENT_TYPE,
   size_t PERMISSIONS,
+  bool PACK,
+  bool MIGRATE,
+>
+struct set_topology_handle_base__ :
+public data_client_handle__<DATA_CLIENT_TYPE, PERMISSIONS>
+{
+  set_topology_buffer_t buffer;
+};
+
+template<
+  typename DATA_CLIENT_TYPE,
+  size_t PERMISSIONS,
   typename DEPENDENT_DATA_CLIENT_TYPE,
   bool PACK,
   bool MIGRATE,
 >
 struct set_topology_handle__ :
-public data_client_handle__<DATA_CLIENT_TYPE, PERMISSIONS>
+public set_topology_handle_base__<DATA_CLIENT_TYPE, PERMISSIONS, PACK, MIGRATE>
 {
   using dependent_data_client_handle_t =
     data_client_handle__<DEPENDENT_DATA_CLIENT_TYPE, PERMISSIONS>;
 
   dependent_data_client_handle_t dependent_handle;
 
-  set_topology_buffer_t buffer;
+};
+
+template<
+  typename DATA_CLIENT_TYPE,
+  size_t PERMISSIONS,
+  bool PACK,
+  bool MIGRATE,
+>
+struct set_topology_handle__<DATA_CLIENT_TYPE, PERMISSIONS, void, PACK,
+  MIGRATE> :
+public set_topology_handle_base__<DATA_CLIENT_TYPE, PERMISSIONS, PACK, MIGRATE>
+{
+
 };
 
 } // namespace flecsi
