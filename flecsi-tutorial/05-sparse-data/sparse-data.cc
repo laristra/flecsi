217536a9075ea0f8a23aa7fa2bab58382d092ed9
Ben Bergen <bergen@lanl.gov>
2018-01-18 13:25:59 -0700

    Working on tutorial.

--- flecsi-tutorial/05-sparse-data/sparse-data.cc
+++ flecsi-tutorial/05-sparse-data/sparse-data.cc
@@ -23,27 +23,27 @@ using namespace flecsi;
 using namespace flecsi::tutorial;
 
 flecsi_register_data_client(mesh_t, clients, mesh);
-flecsi_register_field(mesh_t, hydro, pressure, double, sparse, 1, cells);
+flecsi_register_field(mesh_t, hydro, fractions, double, sparse, 1, cells);
 
 namespace hydro {
 
-void initialize_materials(mesh<ro> mesh, sparse_field_mutator p) {
+void initialize_materials(mesh<ro> mesh, sparse_field_mutator f) {
 
   for(auto c: mesh.cells(owned)) {
     const size_t random = rand()/double(RAND_MAX) * 5;
 
     for(size_t i{0}; i<random; ++i) {
-      p(c,i) = i;
+      f(c,i) = i;
     } // for
   } // for
 } // initialize_pressure
 
 flecsi_register_task(initialize_materials, hydro, loc, single);
 
-void print_materials(mesh<ro> mesh, sparse_field<ro> p) {
-  for(auto i: p.indices()) {
-    for(auto e: p.entries(i)) {
-      std::cout << p(i,e) << " ";
+void print_materials(mesh<ro> mesh, sparse_field<ro> f) {
+  for(auto c: mesh.cells()) {
+    for(auto e: f.entries(c)) {
+      std::cout << f(c,e) << " ";
     } // for
     std::cout << std::endl;
   } // for
@@ -61,15 +61,15 @@ void driver(int argc, char ** argv) {
   auto m = flecsi_get_client_handle(mesh_t, clients, mesh);
 
   {
-  auto p = flecsi_get_mutator(m, hydro, pressure, double, sparse, 0, 5);
+  auto f = flecsi_get_mutator(m, hydro, fractions, double, sparse, 0, 5);
 
-  flecsi_execute_task(initialize_materials, hydro, single, m, p);
+  flecsi_execute_task(initialize_materials, hydro, single, m, f);
   } // scope
 
   {
-  auto p = flecsi_get_handle(m, hydro, pressure, double, sparse, 0);
+  auto f = flecsi_get_handle(m, hydro, fractions, double, sparse, 0);
 
-  flecsi_execute_task(print_materials, hydro, single, m, p);
+  flecsi_execute_task(print_materials, hydro, single, m, f);
   } // scope
 
 } // driver
