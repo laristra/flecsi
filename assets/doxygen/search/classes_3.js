932e4fd237cbd0d78c6ef6dd20c5316b3c7cc386
Automatic Deployment (Travis CI) <flecsi-commit@lanl.gov>
2018-02-20 22:58:42 +0000

    Documentation Update

--- assets/doxygen/search/classes_3.js
+++ assets/doxygen/search/classes_3.js
@@ -8,6 +8,7 @@ var searchData=
   ['data_5fclient_5fhandle_5findex_5fsubspace_5ft',['data_client_handle_index_subspace_t',['../structflecsi_1_1data__client__handle__index__subspace__t.html',1,'flecsi']]],
   ['data_5fclient_5finterface_5f_5f',['data_client_interface__',['../structflecsi_1_1data_1_1data__client__interface____.html',1,'flecsi::data']]],
   ['data_5fclient_5fpolicy_5fhandler_5f_5f',['data_client_policy_handler__',['../structflecsi_1_1data_1_1data__client__policy__handler____.html',1,'flecsi::data']]],
+  ['data_5fclient_5fpolicy_5fhandler_5f_5f_3c_20color_5fdata_5fclient_5ft_20_3e',['data_client_policy_handler__&lt; color_data_client_t &gt;',['../structflecsi_1_1data_1_1data__client__policy__handler_____3_01color__data__client__t_01_4.html',1,'flecsi::data']]],
   ['data_5fclient_5fpolicy_5fhandler_5f_5f_3c_20global_5fdata_5fclient_5ft_20_3e',['data_client_policy_handler__&lt; global_data_client_t &gt;',['../structflecsi_1_1data_1_1data__client__policy__handler_____3_01global__data__client__t_01_4.html',1,'flecsi::data']]],
   ['data_5fclient_5fpolicy_5fhandler_5f_5f_3c_20topology_3a_3amesh_5ftopology_5f_5f_3c_20policy_5ftype_20_3e_20_3e',['data_client_policy_handler__&lt; topology::mesh_topology__&lt; POLICY_TYPE &gt; &gt;',['../structflecsi_1_1data_1_1data__client__policy__handler_____3_01topology_1_1mesh__topology_____3_01POLICY__TYPE_01_4_01_4.html',1,'flecsi::data']]],
   ['data_5fclient_5fpolicy_5fhandler_5f_5f_3c_20topology_3a_3aset_5ftopology_5f_5f_3c_20policy_5ftype_20_3e_20_3e',['data_client_policy_handler__&lt; topology::set_topology__&lt; POLICY_TYPE &gt; &gt;',['../structflecsi_1_1data_1_1data__client__policy__handler_____3_01topology_1_1set__topology_____3_01POLICY__TYPE_01_4_01_4.html',1,'flecsi::data']]],
@@ -24,9 +25,9 @@ var searchData=
   ['dense_5fdata_5fhandle_5fbase_5f_5f',['dense_data_handle_base__',['../structflecsi_1_1dense__data__handle__base____.html',1,'flecsi']]],
   ['dense_5fdata_5fhandle_5fbase_5f_5f_3c_20t_2c_20exclusive_5fpermissions_2c_20shared_5fpermissions_2c_20ghost_5fpermissions_20_3e',['dense_data_handle_base__&lt; T, EXCLUSIVE_PERMISSIONS, SHARED_PERMISSIONS, GHOST_PERMISSIONS &gt;',['../structflecsi_1_1dense__data__handle__base____.html',1,'flecsi']]],
   ['dense_5fdata_5fhandle_5fbase_5ft',['dense_data_handle_base_t',['../structflecsi_1_1dense__data__handle__base__t.html',1,'flecsi']]],
+  ['dense_5fhandle_5ft',['dense_handle_t',['../structflecsi_1_1data_1_1hpx_1_1dense__handle__t.html',1,'flecsi::data::hpx']]],
   ['dense_5fhandle_5ft',['dense_handle_t',['../structflecsi_1_1data_1_1legion_1_1dense__handle__t.html',1,'flecsi::data::legion']]],
   ['dense_5fhandle_5ft',['dense_handle_t',['../structflecsi_1_1data_1_1mpi_1_1dense__handle__t.html',1,'flecsi::data::mpi']]],
-  ['dense_5fhandle_5ft',['dense_handle_t',['../structflecsi_1_1data_1_1hpx_1_1dense__handle__t.html',1,'flecsi::data::hpx']]],
   ['dimensioned_5farray_5f_5f',['dimensioned_array__',['../classflecsi_1_1utils_1_1dimensioned__array____.html',1,'flecsi::utils']]],
   ['domain_5fconnectivity_5f_5f',['domain_connectivity__',['../classflecsi_1_1topology_1_1domain__connectivity____.html',1,'flecsi::topology']]],
   ['domain_5fentity_5f_5f',['domain_entity__',['../classflecsi_1_1topology_1_1domain__entity____.html',1,'flecsi::topology']]]
