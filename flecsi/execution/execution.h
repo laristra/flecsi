00ba603a21cc5766cccfe5d2648df2d5f7fe1a56
Marc Charest <charest@lanl.gov>
2018-03-01 12:22:41 -0700

    forgotten namespace

--- flecsi/execution/execution.h
+++ flecsi/execution/execution.h
@@ -295,7 +295,7 @@ clog_register_tag(execution);
   /* Execute the user task */                                                  \
   /* WARNING: This macro returns a future. Don't add terminations! */          \
   flecsi::execution::task_interface_t::execute_task<                           \
-      launch_type_t::launch,                                                   \
+      flecsi::execution::launch_type_t::launch,                                \
       flecsi::utils::const_string_t{EXPAND_AND_STRINGIFY(task)}.hash(),        \
       __flecsi_internal_return_type(task),                                     \
       __flecsi_internal_arguments_type(task)>(__VA_ARGS__)
