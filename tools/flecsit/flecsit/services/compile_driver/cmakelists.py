c39b297b2b5b8d270b9804cf20dddda5049c491e
Li-Ta Lo <lita.ollie@gmail.com>
2018-03-28 15:45:59 -0600

    add installation of io/vtk header files, add c++14 as requirement

--- tools/flecsit/flecsit/services/compile_driver/cmakelists.py
+++ tools/flecsit/flecsit/services/compile_driver/cmakelists.py
@@ -32,6 +32,7 @@ add_executable(${TARGET}
 
 target_compile_definitions(${TARGET} PRIVATE ${FLECSI_DEFINES})
 target_link_libraries(${TARGET} FleCSI ${FLECSI_LIBRARIES})
+target_compile_features(${TARGET} PUBLIC cxx_std_14)
 
 install(TARGETS ${TARGET} DESTINATION ${INSTALL_PREFIX})
 """)
