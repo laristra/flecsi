488e1b4e1331c7cd1b7d845210a226de9f670f3d
Ben Bergen <bergen@lanl.gov>
2018-05-04 23:25:28 -0600

    Working on MPI interoperability example.

--- flecsi-tutorial/08-mpi-interoperability/README.md
+++ flecsi-tutorial/08-mpi-interoperability/README.md
@@ -8,6 +8,16 @@
 
 # MPI Interoperability Example
 
+FleCSI provides straightforward interoperability with libraries and code
+that depend on the MPI runtime. Data that are registered in FleCSI may
+be passed to an MPI task and used as normal. This example demonstrates
+accessing data that have been registered and initialized in FleCSI from
+the MPI runtime. Additionally, this example demonstrates a mechanism for
+creating persistent references from FleCSI to data that are allocated in
+the MPI runtime.
+
+NOTES:
+
 ```cpp
 ```
 
--- flecsi-tutorial/08-mpi-interoperability/mpi-interoperability.cc
+++ flecsi-tutorial/08-mpi-interoperability/mpi-interoperability.cc
@@ -19,15 +19,21 @@
 #include <cstdlib>
 #include <iostream>
 
+#include<flecsi-tutorial/specialization/mesh/mesh.h>
 #include<flecsi/data/data.h>
 #include<flecsi/execution/execution.h>
 #include<flecsi/data/global_accessor.h>
 
 using namespace flecsi;
+using namespace flecsi::tutorial;
+
+// This field will be accessed by both the FleCSI and MPI runtimes.
+
+flecsi_register_field(mesh_t, example, field, double, dense, 1, cells);
 
 // This call registers storage for a pointer address on each color of the
-// runtime. The uintptr_t type is a standard C++ type specifically designed
-// to hold a pointer value.
+// runtime. These data will be used to reference allocations that are
+// created within an MPI task.
 
 flecsi_register_color(ns, name, double *, 1);
 
@@ -36,6 +42,33 @@ namespace example {
 //----------------------------------------------------------------------------//
 // MPI Tasks
 //----------------------------------------------------------------------------//
+
+// This task is executed in the FleCSI runtime.
+
+void initialize_field(mesh<ro> mesh, field<rw> f) {
+  for(auto c: mesh.cells(owned)) {
+    f(c) = double(c->id());
+  } // for
+} // initialize_field
+
+flecsi_register_task(initialize_field, example, loc, single);
+
+// This task is executed in the MPI runtime.
+
+void access_field(mesh<ro> mesh, field<ro> f) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  for(auto c: mesh.cells(owned)) {
+    clog(info) << "cell id: " << c->id() << std::endl;
+    //std::cout << "rank: " << rank << " cell id: " << c->id() << std::endl;
+  } // for
+} // mpi_task
+
+flecsi_register_task(access_field, example, mpi, index);
+
+
+
 
 // Task to allocate data from within the MPI runtime.
 
@@ -90,6 +123,27 @@ namespace execution {
 
 void driver(int argc, char ** argv) {
 
+  // Initialize the dense field data through the FleCSI runtime.
+  {
+  auto m = flecsi_get_client_handle(mesh_t, clients, mesh);
+  auto f = flecsi_get_handle(m, example, field, double, dense, 0);
+
+  flecsi_execute_task(initialize_field, example, single, m, f);
+  } // scope
+
+  // Access the field data through the MPI runtime.
+  {
+  auto m = flecsi_get_client_handle(mesh_t, clients, mesh);
+  auto f = flecsi_get_handle(m, example, field, double, dense, 0);
+
+  flecsi_execute_task(access_field, example, index, m, f);
+  } // scope
+
+
+
+
+
+#if 0
   auto color_handle=flecsi_get_color(ns, name, double *, 0);
 
   flecsi_execute_task(mpi_new, example, index, color_handle);
@@ -98,6 +152,7 @@ void driver(int argc, char ** argv) {
 
   flecsi_execute_task(mpi_print, example, index, color_handle);
   flecsi_execute_task(print, example, single, color_handle);
+#endif
 
 //  flecsi_execute_task(mpi_use, example, index, color_handle, 6.2831852);
 //  flecsi_execute_task(print, example, single, color_handle);
