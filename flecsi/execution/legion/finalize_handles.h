83347d09e193b9a35d2a2696484356bca85f036c
Nick Moss <nickm@lanl.gov>
2018-06-01 12:22:18 -0700

    Legion sparse data fix

--- flecsi/execution/legion/finalize_handles.h
+++ flecsi/execution/legion/finalize_handles.h
@@ -64,9 +64,9 @@ struct finalize_handles_t : public utils::tuple_walker__<finalize_handles_t> {
     
     std::memcpy(h.entries_data[0], h.entries,
                 md->num_exclusive_entries * sizeof(entry_value_t));
-    
+
     std::memcpy(h.entries_data[1],
-                h.entries + md->reserve * sizeof(entry_value_t),
+                h.entries + md->reserve,
                 md->num_shared * sizeof(entry_value_t) * 
                 md->max_entries_per_index);
   }
@@ -103,7 +103,7 @@ struct finalize_handles_t : public utils::tuple_walker__<finalize_handles_t> {
     auto md = static_cast<sparse_field_data_t*>(h.metadata);
     md->num_exclusive_entries = h.num_exclusive_entries();
     md->initialized = true;
-    
+
     std::memcpy(h.offsets_data[0], h.offsets,
                 h.num_exclusive() * sizeof(offset_t));
     
