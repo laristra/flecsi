fb9bb79cf7bccdb02dd58ccca33f7b0715f1ce53
Ben Bergen <bergen@lanl.gov>
2018-07-10 19:43:22 -0400

    Updated documentation.

--- config/project.cmake
+++ config/project.cmake
@@ -31,6 +31,12 @@ else()
 endif()
 
 #------------------------------------------------------------------------------#
+# Automatic version creation.
+#------------------------------------------------------------------------------#
+
+include(version)
+
+#------------------------------------------------------------------------------#
 # Set header suffix regular expression
 #------------------------------------------------------------------------------#
 
@@ -44,8 +50,68 @@ set(CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_STANDARD 17)
 
 #------------------------------------------------------------------------------#
-# This variable is used to collect library and include dependencies for
-# the FleCSIConfig file below.
+# Create user guide header with version information
+#------------------------------------------------------------------------------#
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/flecsi_ug_header.tex.in
+    ${CMAKE_BINARY_DIR}/doc/flecsi_ug_header.tex)
+
+#------------------------------------------------------------------------------#
+# Pandoc options for user guide
+#------------------------------------------------------------------------------#
+
+set(ug_pandoc_options
+    "--toc"
+    "--include-in-header=${CMAKE_SOURCE_DIR}/cinch/tex/addtolength.tex"
+    "--include-in-header=${CMAKE_BINARY_DIR}/doc/flecsi_ug_header.tex"
+    "--include-in-header=${CMAKE_CURRENT_SOURCE_DIR}/doc/flecsi_ug_title.tex"
+    "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/firstpageempty.tex"
+)
+
+#------------------------------------------------------------------------------#
+# Add user guide target
+#------------------------------------------------------------------------------#
+
+cinch_add_doc(user-guide flecsi_ug.py "flecsi;auxiliary"
+    flecsi-user-guide-${${PROJECT_NAME}_VERSION}.pdf
+    PANDOC_OPTIONS ${ug_pandoc_options} IMAGE_GLOB "*.pdf"
+)
+
+#------------------------------------------------------------------------------#
+# Create developer guide header with version information
+#------------------------------------------------------------------------------#
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/flecsi_dg_header.tex.in
+    ${CMAKE_BINARY_DIR}/doc/flecsi_dg_header.tex
+)
+
+#------------------------------------------------------------------------------#
+# Pandoc options for developer guide
+#------------------------------------------------------------------------------#
+
+set(dg_pandoc_options
+    "--toc"
+    "--include-in-header=${CMAKE_SOURCE_DIR}/cinch/tex/addtolength.tex"
+    "--include-in-header=${CMAKE_BINARY_DIR}/doc/flecsi_dg_header.tex"
+    "--include-before-body=${CMAKE_CURRENT_SOURCE_DIR}/doc/flecsi_dg_title.tex"
+    "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/firstpageempty.tex"
+)
+
+set(dg_image_list "${CMAKE_SOURCE_DIR}/doc/medium-flecsi.png")
+
+#------------------------------------------------------------------------------#
+# Add developer guide target
+#------------------------------------------------------------------------------#
+
+cinch_add_doc(developer-guide flecsi_dg.py "flecsi;auxiliary"
+    flecsi-developer-guide-${${PROJECT_NAME}_VERSION}.pdf
+    PANDOC_OPTIONS ${dg_pandoc_options} IMAGE_GLOB "*.pdf"
+    IMAGE_LIST ${dg_image_list}
+)
+
+#------------------------------------------------------------------------------#
+# These variables are used to collect library and include dependencies
+# for the FleCSIConfig file below.
 #------------------------------------------------------------------------------#
 
 set(FLECSI_INCLUDE_DEPENDENCIES)
