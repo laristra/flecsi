720c192d39665e56be57f7143956c19d26e83590
Wei Wu <eddy16112@gmail.com>
2018-11-29 15:06:26 -0500

    more fix for mapper

--- flecsi/execution/legion/mapper.h
+++ flecsi/execution/legion/mapper.h
@@ -195,7 +195,6 @@ public:
     const Legion::Task & task,
     const Legion::Mapping::Mapper::MapTaskInput & input,
     Legion::Mapping::Mapper::MapTaskOutput & output) {
- //   DefaultMapper::map_task(ctx, task, input, output);
 
     if((task.tag == MAPPER_COMPACTED_STORAGE) && (task.regions.size() > 0)) {
 
