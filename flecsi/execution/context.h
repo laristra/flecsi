1c7fe8aeb6c261e385d39cee4e9a41d56869b184
Ben Bergen <bergen@lanl.gov>
2018-12-04 16:43:19 -0800

    Adding execution context.

---
+++ flecsi/execution/context.h
@@ -0,0 +1,79 @@
+/*
+    @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+   /@@/////  /@@          @@////@@ @@////// /@@
+   /@@       /@@  @@@@@  @@    // /@@       /@@
+   /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+   /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+   /@@       /@@/@@//// //@@    @@       /@@/@@
+   /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+   //       ///  //////   //////  ////////  //
+
+   Copyright (c) 2016, Los Alamos National Security, LLC
+   All rights reserved.
+                                                                              */
+#pragma once
+
+/*! @file */
+
+namespace flecsi {
+namespace execution {
+
+/*!
+  The context_u type provides a high-level execution context interface that
+  is implemented by the given context policy.
+
+  @tparam CONTEXT_POLICY The backend context policy.
+
+  @ingroup execution
+ */
+
+template<class CONTEXT_POLICY>
+struct context_u : public CONTEXT_POLICY {
+
+  /*!
+    Meyer's singleton instance.
+   */
+
+  static context_u & instance() {
+    static context_u context;
+    return context;
+  } // instance
+
+private:
+
+  context_u() : CONTEXT_POLICY() {}
+
+  ~context_u() {
+  } // ~context_u
+
+  // These are deleted because this type is a singleton, i.e.,
+  // we don't want anyone to be able to make copies or references.
+
+  context_u(const context_u &) = delete;
+  context_u & operator=(const context_u &) = delete;
+  context_u(context_u &&) = delete;
+  context_u & operator=(context_u &&) = delete;
+
+}; // struct context_u
+
+} // namespace execution
+} // namespace flecsi
+
+// This include file defines the FLECSI_RUNTIME_CONTEXT_POLICY used below.
+
+#include <flecsi/runtime/flecsi_runtime_context_policy.h>
+
+namespace flecsi {
+namespace execution {
+
+/*!
+  The context_t type is the high-level interface to the FleCSI execution
+  context.
+
+  @ingroup execution
+ */
+
+using context_t = context_u<FLECSI_RUNTIME_CONTEXT_POLICY>;
+
+} // namespace execution
+} // namespace flecsi
