acee9a909227cdc620d3f65d55ed0293a4dab7fe
Ben Bergen <ben@medable.com>
2019-02-24 22:14:23 -0700

    Intermediate check-in.
    - Need to finish changing phase to control-point.

--- flecsi/control/phase_walker.h
+++ flecsi/control/phase_walker.h
@@ -29,26 +29,26 @@ namespace flecsi {
 namespace control {
 
 /*!
-  Convenience type for defining phases.
+  Convenience type for defining control points.
  */
 
-template<size_t PHASE>
-using phase_ = flecsi::utils::typeify_u<size_t, PHASE>;
+template<size_t CONTROL_POINT>
+using control_point_ = flecsi::utils::typeify_u<size_t, CONTROL_POINT>;
 
 /*!
   Allow users to define cyclic control points. Cycles can be nested.
 
-  @tparam PREDICATE  A predicate function that determines when
-                     the cycle should end.
-  @tparam PHASES ... A variadic list of phases within the cycle.
+  @tparam PREDICATE      A predicate function that determines when
+                         the cycle should end.
+  @tparam CONTROL_POINTS A variadic list of control points within the cycle.
 
   @ingroup control
  */
 
-template<bool (*PREDICATE)(), typename... PHASES>
+template<bool (*PREDICATE)(), typename... CONTROL_POINTS>
 struct cycle_u {
 
-  using TYPE = std::tuple<PHASES...>;
+  using TYPE = std::tuple<CONTROL_POINTS...>;
 
   using BEGIN_TYPE = typename std::tuple_element<0, TYPE>::type;
   using END_TYPE =
@@ -64,35 +64,35 @@ struct cycle_u {
 }; // struct cycle_u
 
 /*!
-  The phase_walker_u class allows execution of statically-defined
+  The control_point_walker_u class allows execution of statically-defined
   control points.
 
   @ingroup control
  */
 
 template<typename CONTROL_POLICY>
-struct phase_walker_u
-  : public flecsi::utils::tuple_walker_u<phase_walker_u<CONTROL_POLICY>> {
-  phase_walker_u(int argc, char ** argv) : argc_(argc), argv_(argv) {}
+struct control_point_walker_u
+  : public flecsi::utils::tuple_walker_u<control_point_walker_u<CONTROL_POLICY>> {
+  control_point_walker_u(int argc, char ** argv) : argc_(argc), argv_(argv) {}
 
   /*!
-    Handle the tuple type \em PHASE_TYPE.
+    Handle the tuple type \em ELEMENT_TYPE.
 
-    @tparam PHASE_TYPE The phase type. This can either be a size_t
-                       or a \em cycle. Cycles are defined by the
-                       specialization and must conform to the
-                       interface used in the appropriate handle_type
-                       method.
+    @tparam ELEMENT_TYPE The tuple element type. This can either be a size_t
+                         or a \em cycle. Cycles are defined by the
+                         specialization and must conform to the interface used
+                         in the appropriate handle_type method.
    */
 
-  template<typename PHASE_TYPE>
+  template<typename ELEMENT_TYPE>
   void handle_type() {
 
-    if constexpr(std::is_same<typename PHASE_TYPE::TYPE, size_t>::value) {
+    if constexpr(std::is_same<typename ELEMENT_TYPE::TYPE, size_t>::value) {
 
-      // This is not a cycle -> execute each control action for this phase
+      // This is not a cycle -> execute each control action for this
+      // control point.
       auto & sorted =
-        CONTROL_POLICY::instance().sorted_phase_map(PHASE_TYPE::value);
+        CONTROL_POLICY::instance().sorted_control_point_map(ELEMENT_TYPE::value);
 
       for(auto & node : sorted) {
         node.action()(argc_, argv_);
@@ -100,10 +100,11 @@ struct phase_walker_u
     }
     else {
 
-      // This is a cycle -> create a new phase walker to recurse the cycle.
-      while(PHASE_TYPE::predicate()) {
-        phase_walker_u phase_walker(argc_, argv_);
-        phase_walker.template walk_types<typename PHASE_TYPE::TYPE>();
+      // This is a cycle -> create a new control point walker to recurse
+      // the cycle.
+      while(ELEMENT_TYPE::predicate()) {
+        control_point_walker_u control_point_walker(argc_, argv_);
+        control_point_walker.template walk_types<typename ELEMENT_TYPE::TYPE>();
       } // while
     } // if
 
@@ -113,56 +114,55 @@ private:
   int argc_;
   char ** argv_;
 
-}; // struct phase_walker_u
+}; // struct control_point_walker_u
 
 #if defined(FLECSI_ENABLE_GRAPHVIZ)
 
 /*!
-  The phase_writer_u class allows execution of statically-defined
+  The control_point_writer_u class allows execution of statically-defined
   control points.
 
   @ingroup control
  */
 
 template<typename CONTROL_POLICY>
-struct phase_writer_u
-  : public flecsi::utils::tuple_walker_u<phase_writer_u<CONTROL_POLICY>> {
+struct control_point_writer_u
+  : public flecsi::utils::tuple_walker_u<control_point_writer_u<CONTROL_POLICY>> {
   using graphviz_t = flecsi::utils::graphviz_t;
 
-  phase_writer_u(graphviz_t & gv) : gv_(gv) {}
+  control_point_writer_u(graphviz_t & gv) : gv_(gv) {}
 
   /*!
-    Handle the tuple type \em PHASE_TYPE for type size_t.
+    Handle the tuple type \em ELEMENT_TYPE for type size_t.
 
-    @tparam PHASE_TYPE The phase type. This can either be a size_t
-                       or a \em cycle. Cycles are defined by the
-                       specialization and must conform to the
-                       interface used in the appropriate handle_type
-                       method.
+    @tparam ELEMENT_TYPE The tuple element type. This can either be a size_t
+                         or a \em cycle. Cycles are defined by the
+                         specialization and must conform to the interface used
+                         in the appropriate handle_type method.
    */
 
-  template<typename PHASE_TYPE>
+  template<typename ELEMENT_TYPE>
   typename std::enable_if<
-    std::is_same<typename PHASE_TYPE::TYPE, size_t>::value>::type
+    std::is_same<typename ELEMENT_TYPE::TYPE, size_t>::value>::type
   handle_type() {
-    auto & phase_map = CONTROL_POLICY::instance().phase_map(PHASE_TYPE::value);
+    auto & control_point_map = CONTROL_POLICY::instance().control_point_map(ELEMENT_TYPE::value);
 
     // Add the control point node to the graph
     auto * root =
-      gv_.add_node(phase_map.label().c_str(), phase_map.label().c_str());
+      gv_.add_node(control_point_map.label().c_str(), control_point_map.label().c_str());
 
     // Add edge dependency to last control point
-    if(PHASE_TYPE::value > 0) {
-      auto & last = CONTROL_POLICY::instance().phase_map(PHASE_TYPE::value - 1);
+    if(ELEMENT_TYPE::value > 0) {
+      auto & last = CONTROL_POLICY::instance().control_point_map(ELEMENT_TYPE::value - 1);
       auto * last_node = gv_.node(last.label().c_str());
       gv_.add_edge(last_node, root);
     } // if
 
     // Add graph to output
-    phase_map.add(gv_);
+    control_point_map.add(gv_);
 
     // Add edges from graph to control node
-    auto & unsorted = phase_map.nodes();
+    auto & unsorted = control_point_map.nodes();
     for(auto & n : unsorted) {
       if(n.second.edges().size() == 0) {
         gv_.add_edge(root, gv_.node(std::to_string(n.second.hash()).c_str()));
@@ -172,28 +172,27 @@ struct phase_writer_u
   } // handle_type
 
   /*!
-    Handle the tuple type \em PHASE_TYPE for type cycle_u.
+    Handle the tuple type \em ELEMENT_TYPE for type cycle_u.
 
-    @tparam PHASE_TYPE The phase type. This can either be a size_t
-                       or a \em cycle. Cycles are defined by the
-                       specialization and must conform to the
-                       interface used in the appropriate handle_type
-                       method.
+    @tparam ELEMENT_TYPE The tuple element type. This can either be a size_t
+                         or a \em cycle. Cycles are defined by the
+                         specialization and must conform to the interface used
+                         in the appropriate handle_type method.
    */
 
-  template<typename PHASE_TYPE>
+  template<typename ELEMENT_TYPE>
   typename std::enable_if<
-    !std::is_same<typename PHASE_TYPE::TYPE, size_t>::value>::type
+    !std::is_same<typename ELEMENT_TYPE::TYPE, size_t>::value>::type
   handle_type() {
 
-    phase_writer_u phase_writer(gv_);
-    phase_writer.template walk_types<typename PHASE_TYPE::TYPE>();
+    control_point_writer_u control_point_writer(gv_);
+    control_point_writer.template walk_types<typename ELEMENT_TYPE::TYPE>();
 
     // Add edges for cycles and beautify them...
 
-    if(PHASE_TYPE::begin != PHASE_TYPE::end) {
-      auto & begin = CONTROL_POLICY::instance().phase_map(PHASE_TYPE::begin);
-      auto & end = CONTROL_POLICY::instance().phase_map(PHASE_TYPE::end);
+    if(ELEMENT_TYPE::begin != ELEMENT_TYPE::end) {
+      auto & begin = CONTROL_POLICY::instance().control_point_map(ELEMENT_TYPE::begin);
+      auto & end = CONTROL_POLICY::instance().control_point_map(ELEMENT_TYPE::end);
       auto * edge = gv_.add_edge(
         gv_.node(end.label().c_str()), gv_.node(begin.label().c_str()));
       gv_.set_edge_attribute(edge, "label", "cycle");
@@ -208,7 +207,7 @@ struct phase_writer_u
 private:
   graphviz_t & gv_;
 
-}; // struct phase_writer_u
+}; // struct control_point_writer_u
 
 #endif // FLECSI_ENABLE_GRAPHVIZ
 
