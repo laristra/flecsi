c82f999f6b65158c68ffd7db0a1a103fa893e26d
Automatic Deployment (Travis CI) <flecsi-commit@lanl.gov>
2019-03-12 01:17:17 +0000

    Documentation Update

---
+++ assets/doxygen/search/all_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['finalize_5fruntimes',['finalize_runtimes',['../structflecsi_1_1control_1_1runtime__t.html#a462cb01a6cae241525b18291ae337f82',1,'flecsi::control::runtime_t']]],
+  ['flecsi_5fregister_5fcontrol_5faction',['flecsi_register_control_action',['../macro_8h.html#a5fd60faa32e3b29da7e298533d3eb7a8',1,'macro.h']]],
+  ['flecsi_5fregister_5fruntime_5fdriver',['flecsi_register_runtime_driver',['../runtime_8h.html#a8e102c4494a17d715a21b2ba706d07e8',1,'runtime.h']]]
+];
---
+++ assets/doxygen/search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['runtime_5fhandler_5ft',['runtime_handler_t',['../structflecsi_1_1control_1_1runtime__handler__t.html',1,'flecsi::control']]],
+  ['runtime_5ft',['runtime_t',['../structflecsi_1_1control_1_1runtime__t.html',1,'flecsi::control']]]
+];
