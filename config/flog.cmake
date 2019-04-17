a406a444ad6a81de73aef9d1905bb532ea6a0507
Ben Bergen <bergen@lanl.gov>
2019-04-17 14:41:49 -0600

    Updated config.

--- config/flog.cmake
+++ config/flog.cmake
@@ -21,7 +21,7 @@ cmake_dependent_option(FLOG_ENABLE_COLOR_OUTPUT
 mark_as_advanced(FLOG_ENABLE_COLOR_OUTPUT)
 
 cmake_dependent_option(FLOG_ENABLE_TAGS "Enable tag groups" ON
-  "ENABLE_FLOG;ENABLE_BOOST" OFF)
+  "ENABLE_FLOG" OFF)
 mark_as_advanced(FLOG_ENABLE_TAGS)
 
 cmake_dependent_option(FLOG_ENABLE_MPI "Enable flog MPI support" ON
