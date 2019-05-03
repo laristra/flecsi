ac8e54305f7390a15d04ee5145bdf8785724646a
Ben Bergen <bergen@lanl.gov>
2019-05-03 19:51:55 +0000

    Added handlers for storage class views.
    - These files were called init_handles and finalize_handles.

---
+++ flecsi/execution/legion/enactment/init_views.h
@@ -0,0 +1,102 @@
+/*
+    @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+   /@@/////  /@@          @@////@@ @@////// /@@
+   /@@       /@@  @@@@@  @@    // /@@       /@@
+   /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+   /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+   /@@       /@@/@@//// //@@    @@       /@@/@@
+   /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+   //       ///  //////   //////  ////////  //
+
+   Copyright (c) 2016, Triad National Security, LLC
+   All rights reserved.
+                                                                              */
+#pragma once
+
+/*! @file */
+
+#if !defined(__FLECSI_PRIVATE__)
+#error Do not include this file directly!
+#else
+#include <flecsi/data/common/privilege.h>
+#include <flecsi/data/legion/storage_classes.h>
+#include <flecsi/execution/context.h>
+#include <flecsi/utils/demangle.h>
+#include <flecsi/utils/tuple_walker.h>
+#endif
+
+#include <flecsi-config.h>
+
+#if !defined(FLECSI_ENABLE_LEGION)
+#error FLECSI_ENABLE_LEGION not defined! This file depends on Legion!
+#endif
+
+#include <legion.h>
+
+flog_register_tag(init_views);
+
+namespace flecsi {
+namespace execution {
+namespace legion {
+
+/*!
+  The init_views_t type is called to walk the user task arguments inside of
+  an executing legion task to properly complete the users accessors, i.e., by
+  pointing the accessor \em view instances to the appropriate legion-mapped
+  buffers.
+ */
+
+struct init_views_t : public flecsi::utils::tuple_walker_u<init_views_t> {
+
+  /*!
+    Construct an init_views_t instance.
+
+    @param runtime The Legion task runtime.
+    @param context The Legion task runtime context.
+   */
+
+  init_views_t(Legion::Runtime * runtime,
+    Legion::Context & context,
+    std::vector<Legion::PhysicalRegion> const & regions,
+    std::vector<Legion::Future> const & futures)
+    : runtime_(runtime), context_(context), regions_(regions), futures_(futures) {}
+
+  /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*
+    The following methods are specializations on storage class and client
+    type, potentially for every permutation thereof.
+   *^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
+  /*--------------------------------------------------------------------------*
+    Global Topology
+   *--------------------------------------------------------------------------*/
+
+  template<typename DATA_TYPE, size_t PRIVILEGES>
+  void visit(global_topology::accessor_u<DATA_TYPE, PRIVILEGES> & accessor) {
+  } // visit
+
+  /*--------------------------------------------------------------------------*
+    Non-FleCSI Data Types
+   *--------------------------------------------------------------------------*/
+
+  template<typename DATA_TYPE>
+  static typename std::enable_if_t<
+    !std::is_base_of_v<data::data_reference_base_t, DATA_TYPE>>
+  visit(DATA_TYPE &) {
+    {
+      flog_tag_guard(init_views);
+      flog(internal) << "Skipping argument with type "
+                     << flecsi::utils::type<DATA_TYPE>() << std::endl;
+    }
+  } // visit
+
+private:
+  Legion::Runtime * runtime_;
+  Legion::Context & context_;
+  const std::vector<Legion::PhysicalRegion> & regions_;
+  const std::vector<Legion::Future> & futures_;
+
+}; // struct init_views_t
+
+} // namespace legion
+} // namespace execution
+} // namespace flecsi
