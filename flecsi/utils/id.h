698507f529db94197ecbdfc6de4c7c2157df0866
Martin Frank Staley <staley@lanl.gov>
2019-05-17 18:15:54 -0600

    Chucked id_'s FLAGS_UNMASK and its use in operator==, which was probably wrong. Rearranged a few lines and functions, for consistent ordering + clarity. Removed some commented-out code, given that the earlier CI passed. Made operator== and operator!= consistent with one another. Updated unit test and id.blessed* files accordingly.

--- flecsi/utils/id.h
+++ flecsi/utils/id.h
@@ -39,14 +39,10 @@ public:
   static_assert(PBITS + EBITS + FBITS + GBITS + 4 == 128,
     "invalid id bit configuration");
 
-  // FLAGS_UNMASK's "<< 59" would seem to require this... - martin
   static_assert(sizeof(std::size_t) * CHAR_BIT >= 64,
     "need std::size_t >= 64 bit");
 
-  // FLAGS_UNMASK
-  // 1000011111111111 1111111111111111 1111111111111111 1111111111111111
-  static constexpr std::size_t FLAGS_UNMASK =
-    ~(((std::size_t(1) << FBITS) - std::size_t(1)) << 59);
+  // Constructors and make()...
 
   id_() = default;
   id_(id_ &&) = default;
@@ -66,91 +62,33 @@ public:
     global_id.domain_ = M;
     global_id.partition_ = partition_id;
     global_id.entity_ = local_id;
-    global_id.global_ = global;
     global_id.flags_ = flags;
+    global_id.global_ = global;
 
     return global_id;
   }
 
-  /*
-  // appears to be unused anywhere in FleCSI
-  template<std::size_t M>
-  static id_ make(const std::size_t dim,
-    const std::size_t local_id,
-    const std::size_t partition_id = 0,
-    const std::size_t flags = 0,
-    const std::size_t global = 0) {
-    id_ global_id;
-    global_id.dimension_ = dim;
-    global_id.domain_ = M;
-    global_id.partition_ = partition_id;
-    global_id.entity_ = local_id;
-    global_id.global_ = global;
-    global_id.flags_ = flags;
+  // Assignment...
 
-    return global_id;
-  }
-  */
+  id_ & operator=(id_ &&) = default;
+  id_ & operator=(const id_ & id) = default;
 
-  /*
-  // appears to be unused anywhere in FleCSI
-  static id_ make(const std::size_t dim,
-    const std::size_t local_id,
-    const std::size_t partition_id = 0,
-    const std::size_t flags = 0,
-    const std::size_t global = 0,
-    const std::size_t domain = 0) {
-    id_ global_id;
-    global_id.dimension_ = dim;
-    global_id.domain_ = domain;
-    global_id.partition_ = partition_id;
-    global_id.entity_ = local_id;
-    global_id.global_ = global;
-    global_id.flags_ = flags;
-
-    return global_id;
-  }
-  */
-
-  // [entity][partition][domain][dimension]
-  //
-  // As id_ is used in FleCSI, local_id() takes this id_, which is:
-  //    dd mm pppppppppppppppppppp eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-  //    ffff gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
-  // and produces:
-  //    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee pppppppppppppppppppp mm dd
-  // as the return value. In short, it tosses flags and global, and reverses
-  // the order of dimension, domain, partition, and entity.
-  local_id_t local_id() const {
-    local_id_t r = dimension_;
-    r |= local_id_t(domain_) << 2;
-    r |= local_id_t(partition_) << 4;
-    r |= local_id_t(entity_) << (4 + PBITS);
-    return r;
-  }
-
-  /*
-  // appears to be unused anywhere in FleCSI
-  std::size_t global_id() const {
-    constexpr std::size_t unmask = ~((std::size_t(1) << EBITS) - 1);
-    return static_cast<std::size_t>((local_id() & unmask) | global_);
-  }
-  */
-
-  void set_global(const std::size_t global) {
-    global_ = global;
-  }
-
-  std::size_t global() const {
-    return global_;
-  }
+  // Setters...
 
   void set_partition(const std::size_t partition) {
     partition_ = partition;
   }
 
-  id_ & operator=(id_ &&) = default;
-  id_ & operator=(const id_ & id) = default;
+  void set_flags(const std::size_t flags) {
+    assert(flags < (1 << FBITS) && "flag bits exceeded");
+    flags_ = flags;
+  }
+
+  void set_global(const std::size_t global) {
+    global_ = global;
+  }
+
+  // Getters...
 
   std::size_t dimension() const {
     return dimension_;
@@ -168,37 +106,49 @@ public:
     return entity_;
   }
 
-  std::size_t index_space_index() const {
-    return entity_;
-  }
-
   std::size_t flags() const {
     return flags_;
   }
 
-  void set_flags(const std::size_t flags) {
-    assert(flags < (1 << FBITS) && "flag bits exceeded");
-    flags_ = flags;
+  std::size_t global() const {
+    return global_;
   }
+
+  // index_space_index(): same as entity getter
+  std::size_t index_space_index() const {
+    return entity_;
+  }
+
+  // Construct "local ID"...
+  // [entity][partition][domain][dimension]
+  //
+  // As id_ is used in FleCSI, local_id() takes this id_, which is:
+  //    dd mm pppppppppppppppppppp eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+  //    ffff gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+  // and produces:
+  //    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee pppppppppppppppppppp mm dd
+  // as the return value. In short, it tosses flags and global, and reverses
+  // the order of dimension, domain, partition, and entity.
+  local_id_t local_id() const {
+    local_id_t r = dimension_;
+    r |= local_id_t(domain_) << 2;
+    r |= local_id_t(partition_) << 4;
+    r |= local_id_t(entity_) << (4 + PBITS);
+    return r;
+  }
+
+  // Comparison (<, ==, !=)...
 
   bool operator<(const id_ & id) const {
     return local_id() < id.local_id();
   }
 
   bool operator==(const id_ & id) const {
-    // Each side of the below comparison amounts to:
-    //    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee pppppppppppppppppppp mm dd
-    //    &
-    //    1000011111111111111111111111111111111111 11111111111111111111 11 11
-    // I haven't yet determined if/why that makes sense. -Martin
-
-    return (local_id() & FLAGS_UNMASK) == (id.local_id() & FLAGS_UNMASK);
+    return local_id() == id.local_id();
   }
 
-  // As written, the following makes more sense than == does.
-  // However, != != !(==), which is very strange, and possibly unintentional.
   bool operator!=(const id_ & id) const {
-    return !(local_id() == id.local_id());
+    return !(*this == id);
   }
 
 private:
