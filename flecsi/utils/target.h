c743b768c9fa94515ed370ce96e95d0749f26b2f
Ben Bergen <bergen@lanl.gov>
2019-06-04 05:05:12 +0000

    Added macro for compile target logic.
    - This mostly just picks up defines from Kokkos.

---
+++ flecsi/utils/target.h
@@ -0,0 +1,46 @@
+/*
+    @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+   /@@/////  /@@          @@////@@ @@////// /@@
+   /@@       /@@  @@@@@  @@    // /@@       /@@
+   /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+   /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+   /@@       /@@/@@//// //@@    @@       /@@/@@
+   /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+   //       ///  //////   //////  ////////  //
+
+   Copyright (c) 2016, Triad National Security, LLC
+   All rights reserved.
+                                                                              */
+#pragma once
+
+/*! @file */
+
+#include <flecsi-config.h>
+
+#if !defined(__FLECSI_PRIVATE__)
+#error Do not include this file directly!
+#endif
+
+//----------------------------------------------------------------------------//
+// Pickup Kokkos defines if enabled.
+//----------------------------------------------------------------------------//
+
+#if defined(FLECSI_ENABLE_KOKKOS)
+#include <Kokkos.hpp>
+
+#define FLECSI_TARGET KOKKOS_FUNCTION
+#define FLECSI_INLINE_TARGET KOKKOS_INLINE_FUNCTION
+
+#endif // FLECSI_ENABLE_KOKKOS
+
+//----------------------------------------------------------------------------//
+// Defaults.
+//----------------------------------------------------------------------------//
+
+#if !defined(FLECSI_TARGET)
+#define FLECSI_TARGET
+#endif
+
+#if !defined(FLECSI_INLINE_TARGET)
+#define FLECSI_INLINE_TARGET inline
+#endif
