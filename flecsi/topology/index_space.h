5df81dc8a005cae482d1dc992b185d8d6f82d605
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-06-07 13:12:13 -0500

    Minor changes to make client_registration test compile on MSVC

--- flecsi/topology/index_space.h
+++ flecsi/topology/index_space.h
@@ -778,6 +778,10 @@ public:
     return *this;
   }
 
+#if !defined(_MSC_VER)
+  // MSVC fails to compile the cast() functions is the variadic template
+  // parameters are defaulted
+
   //-----------------------------------------------------------------//
   //! Cast an index space by re-specifying template parameters such
   //! as OWNED or SORTED.
@@ -819,6 +823,43 @@ public:
     assert(res != nullptr && "invalid cast");
     return *res;
   }
+#else
+  //-----------------------------------------------------------------//
+  //! Cast an index space by re-specifying template parameters such
+  //! as OWNED or SORTED.
+  //-----------------------------------------------------------------//
+  template<class S,
+    bool STORAGE2 = STORAGE,
+    bool OWNED2 = OWNED,
+    bool SORTED2 = SORTED,
+    class F2 = F>
+  auto & cast() {
+    static_assert(std::is_convertible<S, T>::value, "invalid index space cast");
+
+    auto res = reinterpret_cast<index_space_u<S, STORAGE2, OWNED2, SORTED2, F2,
+      ID_STORAGE_TYPE, STORAGE_TYPE> *>(this);
+    assert(res != nullptr && "invalid cast");
+    return *res;
+  }
+
+  //-----------------------------------------------------------------//
+  //! Cast an index space by re-specifying template parameters such
+  //! as OWNED or SORTED.
+  //-----------------------------------------------------------------//
+  template<class S,
+    bool STORAGE2 = STORAGE,
+    bool OWNED2 = OWNED,
+    bool SORTED2 = SORTED,
+    class F2 = F>
+  auto & cast() const {
+    static_assert(std::is_convertible<S, T>::value, "invalid index space cast");
+
+    auto res = reinterpret_cast<index_space_u<S, STORAGE2, OWNED2, SORTED2, F2,
+      ID_STORAGE_TYPE, STORAGE_TYPE> const *>(this);
+    assert(res != nullptr && "invalid cast");
+    return *res;
+  }
+#endif
 
   //-----------------------------------------------------------------//
   //! Return begin iterator
