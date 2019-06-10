cf98ecbd3eeceb97d5439a2862199036c0e4b0cc
Ben Bergen <bergen@lanl.gov>
2019-06-10 21:19:46 +0000

    Added stubs for Wei to do I/O.

--- flecsi/io/io_interface.h
+++ flecsi/io/io_interface.h
@@ -14,3 +14,21 @@
 #pragma once
 
 /*!  @file */
+
+template<typename IO_POLICY>
+struct io_interface_u {
+}; // struct io_interface_u
+
+//----------------------------------------------------------------------------//
+// This include file defines the FLECSI_RUNTIME_IO_POLICY used below.
+//----------------------------------------------------------------------------//
+
+#include <flecsi/runtime/io_policy.h>
+
+namespace flecsi {
+namespace io {
+
+using io_interface_t = io_interface_u<FLECSI_RUNTIME_IO_POLICY>;
+
+} // namespace io
+} // namespace flecsi
---
+++ flecsi/io/test/io.cc
@@ -0,0 +1,37 @@
+/*
+    @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+   /@@/////  /@@          @@////@@ @@////// /@@
+   /@@       /@@  @@@@@  @@    // /@@       /@@
+   /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+   /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+   /@@       /@@/@@//// //@@    @@       /@@/@@
+   /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+   //       ///  //////   //////  ////////  //
+
+   Copyright (c) 2016, Triad National Security, LLC
+   All rights reserved.
+                                                                              */
+
+#define __FLECSI_PRIVATE__
+#include <flecsi/execution/execution.h>
+#include <flecsi/io/io_interface.h>
+#include <flecsi/utils/ftest.h>
+
+using namespace flecsi;
+
+namespace io_test {
+
+void check() {
+
+  FTEST();
+
+} // check
+
+flecsi_register_task(check, io_test, loc, index);
+
+} // namespace io_test
+
+int io_sanity(int argc, char ** argv) {
+} // io_sanity
+
+ftest_register_driver(io_sanity);
