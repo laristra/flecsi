e2e549ec04786466f7da07afd149f211810141c8
Ben Bergen <bergen@lanl.gov>
2019-06-10 20:49:05 +0000

    Landing pad for Wei.

--- flecsi/io/io.h
+++ flecsi/io/io.h
@@ -13,31 +13,4 @@
                                                                               */
 #pragma once
 
-/*!
-  @file
-
- */
-
-/*----------------------------------------------------------------------------*
-
- *----------------------------------------------------------------------------*/
-
-#define flecsi_register_simulation(name)
-
-/*!
-  @def flecsi_register_output_group
-
-  @ingroup data
- */
-
-#define flecsi_register_output_group(name, ...) /* MACRO IMPLEMENTATION */
-
-/*!
-  @def flecsi_write_output_group
-
-  @ingroup data
- */
-
-#define flecsi_write_output_group(name, stamp) /* MACRO IMPLEMENTATION */
-
-#define flecsi_write_output(name, stamp, ...)
+/*!  @file */
