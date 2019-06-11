164c885ce8c9139c758df2cb41fa0de4afa83644
Automatic Deployment (Travis CI) <flecsi-commit@lanl.gov>
2019-06-11 01:58:10 +0000

    Documentation Update

--- doxygen/search/all_a.js
+++ doxygen/search/all_a.js
@@ -16,6 +16,7 @@ var searchData=
   ['legion_5ffuture_5fu_3c_20return_2c_20launch_5ftype_5ft_3a_3asingle_20_3e',['legion_future_u&lt; RETURN, launch_type_t::single &gt;',['../structflecsi_1_1execution_1_1legion__future__u_3_01RETURN_00_01launch__type__t_1_1single_01_4.html',1,'flecsi::execution']]],
   ['legion_5ffuture_5fu_3c_20void_2c_20launch_5ftype_5ft_3a_3aindex_20_3e',['legion_future_u&lt; void, launch_type_t::index &gt;',['../structflecsi_1_1execution_1_1legion__future__u_3_01void_00_01launch__type__t_1_1index_01_4.html',1,'flecsi::execution']]],
   ['legion_5ffuture_5fu_3c_20void_2c_20launch_5ftype_5ft_3a_3asingle_20_3e',['legion_future_u&lt; void, launch_type_t::single &gt;',['../structflecsi_1_1execution_1_1legion__future__u_3_01void_00_01launch__type__t_1_1single_01_4.html',1,'flecsi::execution']]],
+  ['legion_5fio_5fpolicy_5ft',['legion_io_policy_t',['../structflecsi_1_1io_1_1legion__io__policy__t.html',1,'flecsi::io']]],
   ['less_5fthan',['less_than',['../simple__id_8h.html#a6091f614969876662d071a02c5e0f8f2',1,'flecsi::utils::detail']]],
   ['lexical_5fcomparison',['lexical_comparison',['../structflecsi_1_1utils_1_1lexical__comparison.html',1,'flecsi::utils']]],
   ['lexical_5fcomparison_3c_20std_3a_3atuple_3c_20args_2e_2e_2e_20_3e_20_3e',['lexical_comparison&lt; std::tuple&lt; Args... &gt; &gt;',['../structflecsi_1_1utils_1_1lexical__comparison_3_01std_1_1tuple_3_01Args_8_8_8_01_4_01_4.html',1,'flecsi::utils']]],
--- doxygen/search/files_8.js
+++ doxygen/search/files_8.js
@@ -7,6 +7,7 @@ var searchData=
   ['interface_2eh',['interface.h',['../ntree_2interface_8h.html',1,'(Global Namespace)'],['../set_2interface_8h.html',1,'(Global Namespace)'],['../structured__mesh_2interface_8h.html',1,'(Global Namespace)'],['../unstructured__mesh_2interface_8h.html',1,'(Global Namespace)']]],
   ['internal_2eh',['internal.h',['../internal_8h.html',1,'']]],
   ['internal_5ftask_2eh',['internal_task.h',['../internal__task_8h.html',1,'']]],
-  ['io_2eh',['io.h',['../io_8h.html',1,'']]],
+  ['io_5finterface_2eh',['io_interface.h',['../io__interface_8h.html',1,'']]],
+  ['io_5fpolicy_2eh',['io_policy.h',['../io_2legion_2io__policy_8h.html',1,'(Global Namespace)'],['../runtime_2io__policy_8h.html',1,'(Global Namespace)']]],
   ['iterator_2eh',['iterator.h',['../iterator_8h.html',1,'']]]
 ];
