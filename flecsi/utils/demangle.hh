36b08dcbf49f249025746e6549cde76c7283b539
Davis Herring <herring@lanl.gov>
2019-06-17 15:04:10 -0600

    Add utils::symbol

--- flecsi/utils/demangle.hh
+++ flecsi/utils/demangle.hh
@@ -58,5 +58,17 @@ type(const std::type_info & type_info) {
   return demangle(type_info.name());
 } // type
 
+/// Dummy class template.
+/// \tparam S a reference to a function or variable
+template<auto &S> struct Symbol {};
+/// Return the name of the template argument.
+/// \tparam a reference to a function or variable
+/// \return demangled name
+template<auto &S> std::string symbol() {
+  constexpr int PFX=sizeof("flecsi::utils::Symbol<")-1;
+  const auto s=type<Symbol<S>>();
+  return s.substr(PFX,s.size()-1-PFX);
+}
+
 } // namespace utils
 } // namespace flecsi
