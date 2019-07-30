756b2631b5b4b026008591f3e041d24c29b8d332
Davis Herring <herring@lanl.gov>
2019-07-30 14:19:26 -0600

    Remove disabled field_interface.hh client

--- flecsi/data/test/global.cc
+++ flecsi/data/test/global.cc
@@ -12,43 +12,6 @@
    All rights reserved.
                                                                               */
 
-#if 0
-#define __FLECSI_PRIVATE__
-#include <flecsi/data/data.hh>
-#include <flecsi/execution/execution.hh>
-#include <flecsi/utils/demangle.hh>
-#include <flecsi/utils/ftest.hh>
-
-using namespace flecsi;
-
-namespace {
-const data::field_interface_t::global_field<double> gfld(2);
-const auto th = gfld(flecsi_global_topology);
-} // namespace
-
-template<size_t PRIVILEGES>
-using global_accessor_u =
-  flecsi::data::global_accessor_u<double, privilege_pack_u<PRIVILEGES>::value>;
-
-namespace global_test {
-
-void
-global_task(global_accessor_u<rw> ga, double value) {
-  ga = value;
-} // global_task
-
-// flecsi_register_task(global_task, global_test, loc, single);
-
-void
-print(global_accessor_u<ro> ga) {
-  flog(info) << "Value: " << ga << std::endl;
-} // print
-
-// flecsi_register_task(print, global_test, loc, single);
-
-} // namespace global_test
-#endif
-
 #define __FLECSI_PRIVATE__
 #include <flecsi/data/data.hh>
 #include <flecsi/execution/execution.hh>
