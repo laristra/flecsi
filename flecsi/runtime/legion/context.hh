6bfd683b82a3b3513c9b9389c8e856ebcb1d5749
Davis Herring <herring@lanl.gov>
2019-08-08 22:20:02 -0600

    Rename runtime_data_t namespaces

--- flecsi/runtime/legion/context.hh
+++ flecsi/runtime/legion/context.hh
@@ -412,8 +412,8 @@ private:
     Runtime data.
    *--------------------------------------------------------------------------*/
 
-  global_topology::runtime_data_t global_topology_instance_;
-  std::unordered_map<size_t, index_topology::runtime_data_t>
+  global_topo::runtime_data_t global_topology_instance_;
+  std::unordered_map<size_t, index_topo::runtime_data_t>
     index_topology_instances_;
   std::unordered_map<size_t, unstructured_mesh::runtime_data_t>
     unstructured_mesh_topology_instances_;
