20d6a95db5679d164aff0b7395ea0fded1102399
Ben Bergen <bergen@lanl.gov>
2019-08-19 20:15:19 -0600

    Update for Davis' changes.

--- flecsi/data/legion/topologies.hh
+++ flecsi/data/legion/topologies.hh
@@ -176,10 +176,25 @@ struct topology_instance<structured_mesh_topology<POLICY_TYPE>> {
 template<typename POLICY_TYPE>
 struct topology_instance<unstructured_mesh_topology<POLICY_TYPE>> {
 
-  using topology_reference_t =
-    topology_reference<unstructured_mesh_topology<POLICY_TYPE>>;
+  using topology_t = topology::unstructured_mesh_topology<POLICY_TYPE>;
+  using topology_reference_t = topology_reference<topology_t>;
   using coloring_t =
     typename topology::unstructured_mesh_topology_base_t::coloring_t;
+
+#if 0
+  struct entity_walker_t : public utils::tuple_walker<index_walker_t> {
+
+    entity_walker_t(coloring_t const & coloring) : coloring_(coloring) {}
+
+    template<typename ENTITY_TYPE>
+    void visit_type() {
+    } // visit_type
+
+  private:
+    coloring_t coloring_;
+
+  }; // struct entity_walker_t
+#endif
 
   static void create(topology_reference_t const & topology_reference,
     coloring_t const & coloring) {
@@ -189,8 +204,7 @@ struct topology_instance<unstructured_mesh_topology<POLICY_TYPE>> {
     auto & flecsi_context = runtime::context_t::instance();
 
     auto & dense_field_info_store = flecsi_context.get_field_info_store(
-      topology::id<topology::unstructured_mesh_topology<POLICY_TYPE>>(),
-      storage_label_t::dense);
+      topology::id<topology_t>(), storage_label_t::dense);
 
 #if 0
     for(size_t is{0}; is<coloring.index_spaces; ++is) {
