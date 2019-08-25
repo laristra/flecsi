8e5bd27b37f52a05fb888079090996424a5201e3
Ben Bergen <bergen@lanl.gov>
2019-08-25 19:17:59 +0000

    WIP: Adding back mesh coloring.

---
+++ flecsi/topology/unstructured_mesh/coloring.hh
@@ -0,0 +1,91 @@
+/*
+    @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+   /@@/////  /@@          @@////@@ @@////// /@@
+   /@@       /@@  @@@@@  @@    // /@@       /@@
+   /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+   /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+   /@@       /@@/@@//// //@@    @@       /@@/@@
+   /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+   //       ///  //////   //////  ////////  //
+
+   Copyright (c) 2016, Los Alamos National Security, LLC
+   All rights reserved.
+                                                                              */
+#pragma once
+
+/*! @file */
+
+#if !defined(__FLECSI_PRIVATE__)
+#error Do not include this file directly!
+#else
+#endif
+
+#include <map>
+#include <vector>
+
+namespace flecsi {
+namespace coloring {
+
+/*!
+  Local coloring information for a topological index space.  
+ */
+
+struct index_coloring_t {
+
+  /*!
+    Type for passing information about shared entities.
+   */
+
+  struct shared_info_t {
+    size_t id;
+    size_t color;
+    size_t offset;
+    std::vector<size_t> dependent_colors;
+  }; // struct shared_info_t
+
+  /*!
+    Type for passing information about ghost entities.
+   */
+
+  struct ghost_info_t {
+    size_t id;
+    size_t color;
+  }; // struct ghost_info_t
+
+  /*!
+    Set of mesh ids of the primary (disjoint) coloring. In general, this is the
+    union of exclusive and shared.
+   */
+
+  std::vector<size_t> primary;
+
+  /*!
+    Set of exclusive indices.
+   */
+
+  std::vector<size_t> exclusive;
+
+  /*!
+    Set of shared indices.
+   */
+
+  std::vector<shared_info_t> shared;
+
+  /*!
+    Set of ghost indices.
+   */
+
+  std::vector<ghost_info_t> ghost;
+
+}; // struct index_coloring_t
+
+/*!
+  
+ */
+
+struct coloring_t {
+  std::map<size_t, index_coloring_t> index_colorings;
+}; // struct coloring_t
+
+} // namespace coloring
+} // namespace flecsi
