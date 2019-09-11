62ca01b4475e219a27c3c8f4c5b4f26c4a878bdb
Davis Herring <herring@lanl.gov>
2019-09-11 17:14:05 -0600

    Add preliminary flecsi::reduce for MPI

--- flecsi/execution/mpi/execution_policy.hh
+++ flecsi/execution/mpi/execution_policy.hh
@@ -18,11 +18,33 @@
 #if !defined(__FLECSI_PRIVATE__)
 #error Do not include this file directly!
 #else
+#include "flecsi/utils/function_traits.hh"
+#include "future.hh"
 #include <flecsi/execution/mpi/reduction_wrapper.hh>
 #include <flecsi/utils/flog.hh>
 #endif
 
+#include <type_traits>
+#include <utility> // forward
+
 namespace flecsi {
+
+template<auto & F,
+  size_t LAUNCH_DOMAIN,
+  size_t REDUCTION,
+  size_t ATTRIBUTES,
+  typename... ARGS>
+decltype(auto)
+reduce(ARGS &&... args) {
+  using R = typename utils::function_traits<decltype(F)>::return_type;
+  execution::mpi_future<R> ret;
+  if constexpr(std::is_same_v<R, void>)
+    F(std::forward<ARGS>(args)...);
+  else
+    ret.set(F(std::forward<ARGS>(args)...));
+  return ret;
+}
+
 namespace execution {
 
 //--------------------------------------------------------------------------//
