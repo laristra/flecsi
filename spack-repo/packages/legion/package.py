04e4d145eeb1f15c5baf9920d30dedc2680baa9c
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2019-09-11 10:47:31 -0600

    install legion deps, but load according to runtime

--- spack-repo/packages/legion/package.py
+++ spack-repo/packages/legion/package.py
@@ -43,22 +43,32 @@ class Legion(CMakePackage):
             description='Build on top of ibv conduit for InfiniBand support')
     variant('shared', default=True, description='Build shared libraries')
     variant('hdf5', default=True, description='Enable HDF5 support')
+    variant('build_type', default='Release', values=('Debug', 'Release'),
+            description='The build type to build')
 
-    depends_on("cmake@3.12.4", type='build')
+    depends_on("cmake@3.1:", type='build')
     depends_on("gasnet~aligned-segments~pshm segment-mmap-max='16GB'", when='~mpi')
     depends_on("gasnet~aligned-segments~pshm segment-mmap-max='16GB' +mpi", when='+mpi')
     depends_on("gasnet~aligned-segments~pshm segment-mmap-max='16GB' +ibv", when='+ibv')
     depends_on("hdf5~mpi", when='+hdf5')
 
     def cmake_args(self):
+        cmake_cxx_flags = [
+            '-DPRIVILEGE_CHECKS',
+            '-DBOUNDS_CHECKS',
+            '-DENABLE_LEGION_TLS']
+
         options = [
-            '-DCMAKE_BUILD_TYPE=Debug',
             '-DLegion_USE_GASNet=ON',
             '-DLEGION_USE_CUDA=OFF',
             '-DLEGION_USE_OPENMP=OFF',
             '-DLegion_BUILD_EXAMPLES=ON',
-            '-DCMAKE_CXX_FLAGS=-DPRIVILEGE_CHECKS -DBOUNDS_CHECKS -DENABLE_LEGION_TLS',
             '-DBUILD_SHARED_LIBS=%s' % ('+shared' in self.spec)]
+
+        if self.spec.variants['build_type'].value == 'Debug':
+            cmake_cxx_flags.append('-DDEBUG_REALM', '-DDEBUG_LEGION', '-ggdb')
+
+        options.append('-DCMAKE_CXX_FLAGS=%s' % (" ".join(cmake_cxx_flags)))
 
         if '+mpi' in self.spec:
             options.append('-DGASNet_CONDUIT=mpi')
@@ -69,3 +79,4 @@ class Legion(CMakePackage):
             options.append('-DLegion_USE_HDF5=OFF')
 
         return options
+
