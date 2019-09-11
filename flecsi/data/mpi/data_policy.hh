b4f713c3e3f723fd87cd2f7112dba275a97f35d9
Davis Herring <herring@lanl.gov>
2019-09-11 16:21:25 -0600

    Remove outdated MPI topology interfaces

--- flecsi/data/mpi/data_policy.hh
+++ flecsi/data/mpi/data_policy.hh
@@ -20,31 +20,12 @@
   specializations for accessors and topologies.
  */
 
-#include <flecsi/data/mpi/client_handle_specializations.hh>
+#include <flecsi/data/data_reference.hh>
 
 namespace flecsi {
 namespace data {
 
-struct mpi_data_policy_t {
-
-  /*--------------------------------------------------------------------------*
-    Client Interface.
-   *--------------------------------------------------------------------------*/
-
-  /*
-    Documnetation for this interface is in the top-level client type.
-   */
-
-  template<typename DATA_CLIENT_TYPE, size_t NAMESPACE, size_t NAME>
-  static client_handle<DATA_CLIENT_TYPE, 0> get_client_handle() {
-    using client_handle_specialization_t =
-      mpi::client_handle_specialization<DATA_CLIENT_TYPE>;
-
-    return client_handle_specialization_t::template get_client_handle<NAMESPACE,
-      NAME>();
-  } // get_client_handle
-
-}; // struct mpi_data_policy_t
+struct mpi_data_policy_t {}; // struct mpi_data_policy_t
 
 } // namespace data
 } // namespace flecsi
