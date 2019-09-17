bf431c4a4e10b51a2c012d4c298b7796c4dc30ee
Li-Ta Lo <ollie@lanl.gov>
2019-09-17 16:53:28 -0600

    Add data_policy stubs

--- flecsi/data/mpi/data_policy.hh
+++ flecsi/data/mpi/data_policy.hh
@@ -25,7 +25,36 @@
 namespace flecsi {
 namespace data {
 
-struct mpi_data_policy_t {}; // struct mpi_data_policy_t
+struct mpi_data_policy_t {
+  /*--------------------------------------------------------------------------*
+    Topology Instance Interface.
+   *--------------------------------------------------------------------------*/
+
+  template<typename TOPOLOGY_TYPE, typename... ARGS>
+  static void allocate_coloring(
+    data_reference_base_t const & coloring_reference,
+    ARGS &&... args) {
+    // TBD
+  } // allocate_coloring
+
+  template<typename TOPOLOGY_TYPE>
+  static void deallocate_coloring(
+    data_reference_base_t const & coloring_reference) {
+    // TBD
+  } // deallocate_coloring
+
+  template<typename TOPOLOGY_TYPE>
+  static void allocate(data_reference_base_t const & topology_reference,
+    data_reference_base_t const & coloring_reference) {
+    // TBD
+  } // allocate
+
+  template<typename TOPOLOGY_TYPE>
+  static void deallocate(data_reference_base_t const & topology_reference) {
+    // TBD
+  } // deallocate
+
+}; // struct mpi_data_policy_t
 
 } // namespace data
 } // namespace flecsi
