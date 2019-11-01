20b4c08655e7a8aaa3153eb3cba6a2bc23dce8bb
Charles Ferenbaugh <cferenba@lanl.gov>
2019-11-01 15:28:12 -0600

    Fix HDF5 config to find the right paths

--- cmake/hdf5.cmake
+++ cmake/hdf5.cmake
@@ -15,6 +15,18 @@
 option(ENABLE_HDF5 "Enable HDF5" OFF)
 
 if(ENABLE_HDF5)
+
+  # CMake's FindHDF5 package ignores any path in CMAKE_PREFIX_PATH that
+  # doesn't contain the hdf5-config.cmake file in a certain location.
+  # Some of our HDF5 builds don't have this.  So we have to give CMake
+  # a little extra help...
+  if(NOT HDF5_ROOT)
+    find_path(HDF5_INCLUDE_DIR hdf5.h
+      HINTS ENV HDF5_ROOT
+      PATH_SUFFIXES include)
+    get_filename_component(HDF5_ROOT ${HDF5_INCLUDE_DIR} DIRECTORY)
+  endif()
+
   find_package(HDF5 REQUIRED)
 
   include_directories(${HDF5_C_INCLUDE_DIR})
