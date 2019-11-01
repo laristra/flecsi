3de1e1a2aed57f6bdd9ff98c333761808a9a9899
Irina <irina@lanl.gov>
2019-11-01 13:45:29 -0600

    added check for Legion build with CUDA

--- flecsi/execution/test/kokkos.cc
+++ flecsi/execution/test/kokkos.cc
@@ -122,7 +122,11 @@ init_toc(client_handle_t<test_mesh_t, ro> mesh,
 #endif
 }
 
+#if defined (REALM_USE_CUDA)
 flecsi_register_task_simple(init_toc, toc, index);
+#else
+flecsi_register_task_simple(init_toc, loc, index);
+#endif
 
 
 void
@@ -152,7 +156,11 @@ test(client_handle_t<test_mesh_t, ro> mesh,
   }; // forall
 }
 
+#if defined (REALM_USE_CUDA)
 flecsi_register_task_simple(test, toc, index);
+#else
+flecsi_register_task_simple(test, loc, index);
+#endif
 //----------------------------------------------------------------------------//
 // Specialization driver.
 //----------------------------------------------------------------------------//
