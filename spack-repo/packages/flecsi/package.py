e2714b3be5e3dcefa440d79d1ba7010ae44e5661
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2019-11-11 22:56:25 -0700

    adding options to flecsi

--- spack-repo/packages/flecsi/package.py
+++ spack-repo/packages/flecsi/package.py
@@ -8,8 +8,7 @@ from spack import *
 
 
 class Flecsi(CMakePackage):
-    '''
-       FleCSI is a compile-time configurable framework designed to support
+    '''FleCSI is a compile-time configurable framework designed to support
        multi-physics application development. As such, FleCSI attempts to
        provide a very general set of infrastructure design patterns that can
        be specialized and extended to suit the needs of a broad variety of
@@ -25,10 +24,27 @@ class Flecsi(CMakePackage):
     version('flecsph', branch='feature/flecsph', submodules=False)
 
     variant('build_type', default='Release', values=('Debug', 'Release'),
-            description='The build type to build')
-    variant('backend', default='mpi', values=('hpx', 'mpi', 'legion'),
-            description='Backend runtime to use for distributed memory')
-    variant('shared', default=True, description='Build shared libraries')
+            description='The build type to build', multi=False)
+    variant('backend', default='mpi', values=('serial','mpi', 'legion','hpx'),
+            description='Backend runtime to use for distributed memory', multi=False)
+    variant('minimal', default=False,
+            description='Disable FindPackageMetis')
+    variant('shared', default=True,
+            description='Build shared libraries')
+    variant('unittest', default=False,
+            description='Enable unit testing')
+    variant('flog', default=False,
+            description='Enable flog testing')
+    variant('doxygen', default=False,
+            description='Enable doxygen')
+    variant('doc', default=False,
+            description='Enable documentation')
+    variant('coverage', default=False,
+            description='Enable coverage build')
+    variant('openmpi', default=True,
+            description='Use OpenMPI package')
+    variant('mpich', default=False,
+            description='Use MPICH package')
     variant('hdf5', default=False,
             description='Enable HDF5 Support')
     variant('caliper', default=False,
@@ -40,14 +56,17 @@ class Flecsi(CMakePackage):
     variant('flecstan', default=False,
             description='Build FleCSI Static Analyzer')
 
-    depends_on('cmake@3.12.1:3.12.4',  type='build')
+    depends_on('cmake@3.12:',  type='build')
     # Requires cinch > 1.0 due to cinchlog installation issue
     #depends_on('cinch@1.01:', type='build')
     depends_on('mpi', when='backend=mpi')
     depends_on('mpi', when='backend=legion')
-    depends_on('hpx', when='backend=hpx')
-    depends_on('legion@ctrl-rep-2 +shared +mpi +hdf5', when='backend=legion +hdf5')
-    depends_on('legion@ctrl-rep-2 +shared +mpi', when='backend=legion ~hdf5')
+    depends_on('mpi', when='backend=hpx')
+    depends_on('openmpi@3.1.3:3.1.4', when='+openmpi')
+    depends_on('mpich@3.2.1', when='+mpich')
+    depends_on('legion@ctrl-rep +shared +mpi +hdf5', when='backend=legion +hdf5')
+    depends_on('legion@ctrl-rep +shared +mpi', when='backend=legion ~hdf5')
+    depends_on('hpx@1.3.0 cxxstd=11', when='backend=hpx')
     depends_on('boost@1.59.0: cxxstd=11 +program_options')
     depends_on('metis@5.1.0:')
     depends_on('parmetis@4.0.3:')
@@ -57,17 +76,27 @@ class Flecsi(CMakePackage):
     depends_on('python@3.0:', when='+tutorial')
     depends_on('llvm', when='+flecstan')
 
-    def cmake_args(self):
-        options = ['-DBUILD_SHARED_LIBS=%s' % ('+shared' in self.spec)]
-        #options.append('-DCINCH_SOURCE_DIR=' + self.spec['cinch'].prefix)
+    conflicts('+tutorial', when='backend=hpx')
+    conflicts('+tutorial', when='@flecsph')
+    conflicts('+hdf5', when='backend=hpx')
+    conflicts('+hdf5', when='backend=mpi')
 
-        if self.spec.variants['build_type'].value == 'Debug':
-            options.append('-DCMAKE_BUILD_TYPE=Debug')
-        elif self.spec.variants['build_type'].value == 'Release':
-            options.append('-DCMAKE_BUILD_TYPE=Release')
+    def cmake_args(self):
+        spec = self.spec
+        options = ['-DCMAKE_DISABLE_FIND_PACKAGE_METIS=%s'%('ON' if '+minimal' in spec else 'OFF'),
+                   '-DBUILD_SHARED_LIBS=%s'%('ON' if '+shared' in spec else 'OFF'),
+                   '-DENABLE_UNIT_TESTS=%s'%('ON' if '+unittest' in spec else 'OFF'),
+                   '-DENABLE_HDF5=%s'%('ON' if '+hdf5' in spec else 'OFF'),
+                   '-DENABLE_CALIPER=%s'%('ON' if '+caliper' in spec else 'OFF'),
+                   '-DENABLE_FLECSTAN=%s'%('ON' if '+flecstan' in spec else 'OFF'),
+                   '-DENABLE_DOXYGEN=%s'%('ON' if '+doxygen' in spec else 'OFF'),
+                   '-DENABLE_DOCUMENTATION=%s'%('ON' if '+doc' in spec else 'OFF'),
+                   '-DENABLE_COVERAGE_BUILD=%s'%('ON' if '+coverage' in spec else 'OFF')]
+                   #'-DENABLE_FLOG=%s'%('ON' if '+flog' in spec else 'OFF'),
+        #options.append('-DCINCH_SOURCE_DIR=' + spec['cinch'].prefix)
 
         # FleCSI for FleCSPH flags
-        if self.spec.satisfies('@flecsph:'):
+        if spec.satisfies('@flecsph:'):
             options.append('-DENABLE_MPI=ON')
             options.append('-DENABLE_OPENMP=ON')
             options.append('-DCXX_CONFORMANCE_STANDARD=c++17')
@@ -76,36 +105,33 @@ class Flecsi(CMakePackage):
             options.append('-DENABLE_FLECSI_TUTORIAL=OFF')
             return options
 
-        if self.spec.variants['backend'].value == 'legion':
+        if spec.variants['build_type'].value == 'Debug':
+            options.append('-DCMAKE_BUILD_TYPE=Debug')
+        elif spec.variants['build_type'].value == 'Release':
+            options.append('-DCMAKE_BUILD_TYPE=Release')
+
+        if spec.variants['backend'].value == 'legion':
             options.append('-DFLECSI_RUNTIME_MODEL=legion')
-        elif self.spec.variants['backend'].value == 'mpi':
+        elif spec.variants['backend'].value == 'mpi':
             options.append('-DFLECSI_RUNTIME_MODEL=mpi')
+        elif spec.variants['backend'].value == 'hpx':
+            options.append('-DFLECSI_RUNTIME_MODEL=hpx')
+            options.append('-DENABLE_MPI=ON')
+            options.append('-DENABLE_OPENMP=ON')
+            options.append('-DCXX_CONFORMANCE_STANDARD=c++17')
+            options.append('-DENABLE_METIS=ON')
+            options.append('-DENABLE_PARMETIS=ON')
+            options.append('-DENABLE_COLORING=ON')
+            options.append('-DENABLE_DEVEL_TARGETS=ON')
         else:
             options.append('-DFLECSI_RUNTIME_MODEL=serial')
-            options.append(
-                '-DENABLE_MPI=OFF',
-            )
+            options.append('-DENABLE_MPI=OFF')
 
-        if '+tutorial' in self.spec:
+        if '+tutorial' in spec:
             options.append('-DENABLE_FLECSIT=ON')
             options.append('-DENABLE_FLECSI_TUTORIAL=ON')
         else:
             options.append('-DENABLE_FLECSIT=OFF')
             options.append('-DENABLE_FLECSI_TUTORIAL=OFF')
-
-        if '+hdf5' in self.spec:
-            options.append('-DENABLE_HDF5=ON')
-        else:
-            options.append('-DENABLE_HDF5=OFF')
-
-        if '+caliper' in self.spec:
-            options.append('-DENABLE_CALIPER=ON')
-        else:
-            options.append('-DENABLE_CALIPER=OFF')
-
-        if '+flecstan' in self.spec:
-            options.append('-DENABLE_FLECSTAN=ON')
-        else:
-            options.append('-DENABLE_FLECSTAN=OFF')
 
         return options
--- spack-repo/packages/legion/package.py
+++ spack-repo/packages/legion/package.py
@@ -26,6 +26,7 @@ class Legion(CMakePackage):
     git      = "https://github.com/StanfordLegion/legion.git"
 
     version('develop', branch='master')
+    version('ctrl-rep', branch='control_replication')
     version('ctrl-rep-2', commit='96682fd8aae071ecd30a3ed5f481a9d84457a4b6')
     version('ctrl-rep-1', commit='a03671b21851d5f0d3f63210343cb61a630f4405')
     version('ctrl-rep-0', commit='177584e77036c9913d8a62e33b55fa784748759c')
