37799a9057aaa477f00fb19d562279569f5c2d6c
Nikunj Gupta <nk.nikunjgupta@gmail.com>
2019-12-12 16:58:40 +0000

    Allow for hpx runtime to build

--- spack-repo/packages/flecsi/package.py
+++ spack-repo/packages/flecsi/package.py
@@ -45,7 +45,8 @@ class Flecsi(CMakePackage):
     depends_on('hpx@master cxxstd=14', when='runtime=hpx')
     depends_on('legion@ctrl-rep-2 +shared +mpi +hdf5', when='runtime=legion +hdf5')
     depends_on('legion@ctrl-rep-2 +shared +mpi', when='runtime=legion ~hdf5')
-    depends_on('boost@1.59.0: cxxstd=11 +program_options')
+    depends_on('boost@1.70.0: cxxstd=14 +program_options', when='runtime=hpx')
+    depends_on('boost@1.59.0: cxxstd=11 +program_options', when='runtime~hpx')
     depends_on('parmetis@4.0.3:')
     depends_on('hdf5', when='+hdf5')
     depends_on('caliper', when='+caliper')
@@ -64,6 +65,9 @@ class Flecsi(CMakePackage):
             options.append('-DFLECSI_RUNTIME_MODEL=legion')
         elif self.spec.variants['runtime'].value == 'mpi':
             options.append('-DFLECSI_RUNTIME_MODEL=mpi')
+        elif self.spec.variants['runtime'].value == 'hpx':
+            options.append('-DFLECSI_RUNTIME_MODEL=hpx')
+            options.append('-DENABLE_MPI=ON')
 
         if '+tutorial' in self.spec:
             options.append('-DENABLE_FLECSIT=ON')
