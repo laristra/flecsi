a43083d4a25d0c6cd19bae49b57d95dc1f9ae990
Nikunj Gupta <nk.nikunjgupta@gmail.com>
2020-01-09 00:56:46 +0000

    minor changes in execution policy

--- flecsi/execution/hpx/policy.hh
+++ flecsi/execution/hpx/policy.hh
@@ -22,6 +22,7 @@
 #include <hpx/include/future.hpp>
 #include <hpx/include/async.hpp>
 
+#include "../launch.hh"
 #include "flecsi/utils/function_traits.hh"
 #include <flecsi/utils/flog.hh>
 
@@ -31,16 +32,18 @@
 namespace flecsi {
 
 template<auto & F,
-  size_t LAUNCH_DOMAIN,
-  size_t REDUCTION,
+  const execution::launch_domain & LAUNCH_DOMAIN,
+  class REDUCTION,
   size_t ATTRIBUTES,
   typename... ARGS>
 decltype(auto)
 reduce(ARGS &&... args) {
   using R = typename utils::function_traits<decltype(F)>::return_type;
+
   hpx::future<R> f = hpx::async(F, std::forward<ARGS>(args)...);
 
   return f.get();
+
 //   execution::mpi_future<R> ret;
 //   if constexpr(std::is_same_v<R, void>)
 //     F(std::forward<ARGS>(args)...);
@@ -49,14 +52,4 @@ reduce(ARGS &&... args) {
 //   return ret;
 }
 
-// namespace execution {
-
-// //--------------------------------------------------------------------------//
-// // Reduction interface.
-// //--------------------------------------------------------------------------//
-
-// template<size_t HASH, typename TYPE>
-// using reduction_wrapper = mpi::reduction_wrapper<HASH, TYPE>;
-
-// } // namespace execution
 } // namespace flecsi
