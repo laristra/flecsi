b07ca9b72fc8374b64adb1493ae5cfeabc7905d0
Ben Bergen <bergen@lanl.gov>
2020-02-05 17:41:12 +0000

    Update doc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0',
+    VERSION: 'devel-2.1.g3b954466d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
