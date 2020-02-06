43fbb9320cb7541f025ad02b23245ee0ceeff99f
R. Tohid <tohid.rod@gmail.com>
2020-02-06 20:35:16 +0000

    Fix `hpx_data_client_handle_policy_t`

--- flecsi/data/hpx/data_client_handle_policy.h
+++ flecsi/data/hpx/data_client_handle_policy.h
@@ -82,9 +82,30 @@ struct data_client_handle_index_subspace_t {
 //! FIXME: Description of class
 //----------------------------------------------------------------------------//
 
-struct hpx_data_client_handle_policy_t {}; // struct data_client_handle_policy_t
-// data_client_handle_index_subspace_t
-// handle_index_subspaces[MAX_INDEX_SUBSPACES];
+struct hpx_data_client_handle_policy_t {
+  // FIXME: This needs to be exposed at a higher level
+
+  // maximum number of adjacencies to read, this limits the size of the
+  // serialize struct passed to Legion
+  // static constexpr size_t MAX_ADJACENCIES = 32;
+  static constexpr size_t MAX_ADJACENCIES = 64;
+
+  // maximum number of handle entities
+  static constexpr size_t MAX_ENTITIES = 7;
+
+  // maximum number of handle index subspaces
+  static constexpr size_t MAX_INDEX_SUBSPACES = 10;
+
+  size_t num_handle_entities;
+  size_t num_handle_adjacencies;
+  size_t num_index_subspaces;
+  data_client_handle_entity_t handle_entities[MAX_ENTITIES];
+  data_client_handle_adjacency_t handle_adjacencies[MAX_ADJACENCIES];
+  data_client_handle_index_subspace_t
+    handle_index_subspaces[MAX_INDEX_SUBSPACES];
+  // data_client_handle_index_subspace_t
+  // handle_index_subspaces[MAX_INDEX_SUBSPACES];
+}; // struct data_client_handle_policy_t
 
 } // namespace flecsi
 
