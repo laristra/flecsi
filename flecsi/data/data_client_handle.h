e33b4a9422dcf1aa365921d056f68bdbd393a434
Davis Herring <herring@lanl.gov>
2020-03-19 15:49:43 -0600

    Remove useless copy constructor declaration

--- flecsi/data/data_client_handle.h
+++ flecsi/data/data_client_handle.h
@@ -56,10 +56,6 @@ struct data_client_handle_base_u : public DATA_CLIENT_TYPE,
       UNMAPPED_PERMISSIONS == 0, "passing mapped client handle to task args");
   }
 
-  data_client_handle_base_u(const data_client_handle_base_u & h)
-    : DATA_POLICY(h), DATA_CLIENT_TYPE(h), type_hash(h.type_hash),
-      name_hash(h.name_hash), namespace_hash(h.namespace_hash) {}
-
   size_t type_hash;
   size_t name_hash;
   size_t namespace_hash;
