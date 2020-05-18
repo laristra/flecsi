83647335d988393c9102cbc39157e8e2ebe74256
Irina <irina@lanl.gov>
2020-05-18 14:36:18 -0600

    updating hpx backand to work with the new iterator

--- flecsi/topology/hpx/set_storage_policy.h
+++ flecsi/topology/hpx/set_storage_policy.h
@@ -37,13 +37,8 @@ struct hpx_set_topology_storage_policy_u {
   static const std::size_t num_index_spaces =
     std::tuple_size<entity_types_t>::value;
 
-  using index_spaces_t = std::array<index_space_u<set_entity_t *,
-                                      true,
-                                      true,
-                                      true,
-                                      void,
-                                      identity_storage_u,
-                                      topology_storage_u>,
+  using index_spaces_t = std::array<
+    index_space_u<set_entity_t, identity_storage_u, entity_storage_t>,
     num_index_spaces>;
 
   index_spaces_t index_spaces;
--- flecsi/topology/hpx/storage_policy.h
+++ flecsi/topology/hpx/storage_policy.h
@@ -1,12 +1,19 @@
-/*~--------------------------------------------------------------------------~*
- * Copyright (c) 2015 Los Alamos National Security, LLC
- * All rights reserved.
- *~--------------------------------------------------------------------------~*/
+/*
+    @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+   /@@/////  /@@          @@////@@ @@////// /@@
+   /@@       /@@  @@@@@  @@    // /@@       /@@
+   /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+   /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+   /@@       /@@/@@//// //@@    @@       /@@/@@
+   /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+   //       ///  //////   //////  ////////  //
 
-#ifndef flecsi_topology_hpx_topology_storage_policy_h
-#define flecsi_topology_hpx_topology_storage_policy_h
+   Copyright (c) 2016, Los Alamos National Security, LLC
+   All rights reserved.
+                                                                              */
+#pragma once
 
-#include "flecsi/topology/mesh_storage.h"
+/*! @file */
 
 #include <array>
 #include <cassert>
@@ -22,16 +29,8 @@
 #include <flecsi/topology/mesh_types.h>
 #include <flecsi/topology/mesh_utils.h>
 
-///
-/// \file
-/// \date Initial file creation: Apr 04, 2017
-///
-
 namespace flecsi {
 namespace topology {
-
-template<size_t, size_t>
-class mesh_entity_t;
 
 ///
 /// \class hpx_data_handle_policy_t data_handle_policy.h
@@ -43,29 +42,16 @@ struct hpx_topology_storage_policy_u {
   static constexpr size_t num_partitions = 5;
   using id_t = utils::id_t;
 
-  using index_spaces_t = std::array<index_space_u<mesh_entity_base_ *,
-                                      true,
-                                      true,
-                                      true,
-                                      void,
-                                      topology_storage_u>,
-    NUM_DIMS + 1>;
+  using storage_t =
+    index_space_u<mesh_entity_base_, topology_storage_u, entity_storage_t>;
 
-  using index_subspaces_t = std::array<index_space_u<mesh_entity_base_ *,
-                                         false,
-                                         true,
-                                         false,
-                                         void,
-                                         topology_storage_u>,
-    NUM_INDEX_SUBSPACES>;
+  using index_spaces_t = std::array<storage_t, NUM_DIMS + 1>;
 
-  using partition_index_spaces_t = std::array<index_space_u<mesh_entity_base_ *,
-                                                false,
-                                                false,
-                                                true,
-                                                void,
-                                                topology_storage_u>,
-    NUM_DIMS + 1>;
+  using index_subspaces_t = std::array<storage_t, NUM_INDEX_SUBSPACES>;
+
+  using partition_index_spaces_t =
+    std::array<index_space_u<mesh_entity_base_, utils::span, entity_storage_t>,
+      NUM_DIMS + 1>;
 
   // array of array of domain_connectivity_u
   std::array<std::array<domain_connectivity_u<NUM_DIMS>, NUM_DOMS>, NUM_DOMS>
@@ -74,6 +60,7 @@ struct hpx_topology_storage_policy_u {
   std::array<index_spaces_t, NUM_DOMS> index_spaces;
 
   index_subspaces_t index_subspaces;
+
   std::array<std::array<partition_index_spaces_t, NUM_DOMS>, num_partitions>
     partition_index_spaces;
 
@@ -84,63 +71,143 @@ struct hpx_topology_storage_policy_u {
     color = context_.color();
   }
 
-  template<size_t D, size_t N>
-  size_t entity_dimension(mesh_entity_t<D, N> *) {
-    return D;
-  }
+  void init_entities(size_t domain,
+    size_t dim,
+    mesh_entity_base_ * entities,
+    utils::id_t * ids,
+    size_t size,
+    size_t num_entities,
+    size_t num_exclusive,
+    size_t num_shared,
+    size_t num_ghost,
+    bool read) {
+    auto & is = index_spaces[domain][dim];
+
+    const auto s = &is.data;
+    s->set_buffer(entities, num_entities, read);
+
+    auto & id_storage = is.ids;
+    id_storage = full_array(ids, num_entities, read);
+
+    for(auto & domain_connectivities : topology) {
+      auto & domain_connectivity_u = domain_connectivities[domain];
+      for(size_t d = 0; d <= NUM_DIMS; ++d) {
+        domain_connectivity_u.get(d, dim).set_entity_storage(*s);
+      } // for
+    } // for
+
+    if(!read) {
+      return;
+    }
+
+    size_t shared_end = num_exclusive + num_shared;
+    size_t ghost_end = shared_end + num_ghost;
+
+    for(size_t partition = 0; partition < num_partitions; ++partition) {
+      auto & isp = partition_index_spaces[partition][domain][dim];
+      isp.data = *s;
+
+      switch(partition_t(partition)) {
+        case exclusive:
+          isp.ids = {ids, num_exclusive};
+          break;
+        case shared:
+          isp.ids = {ids + num_exclusive, ids + shared_end};
+          break;
+        case ghost:
+          isp.ids = {ids + shared_end, ids + ghost_end};
+          break;
+        case owned:
+          isp.ids = {ids, shared_end};
+          break;
+        default:
+          isp.ids = {ids, ghost_end};
+          break;
+      }
+    }
+  } // init_entities
+
+  void init_index_subspace(size_t index_space,
+    size_t index_subspace,
+    size_t domain,
+    size_t dim,
+    utils::id_t * ids,
+    size_t num_entities,
+    bool read) {
+
+    auto & context_ = execution::context_t::instance();
+    auto & ssm = context_.index_subspace_info();
+    auto itr = ssm.find(index_subspace);
+    clog_assert(itr != ssm.end(), "invalid index subspace");
+    const execution::context_t::index_subspace_info_t & si = itr->second;
+
+    auto & is = index_spaces[domain][dim];
+    auto & iss = index_subspaces[index_subspace];
+
+    iss.data = is.data;
+
+    auto & id_storage = iss.ids;
+    id_storage = {{ids, si.capacity}, si.size};
+
+    if(!read) {
+      return;
+    }
+  } // init_index_subspaces
+
+  void init_connectivity(size_t from_domain,
+    size_t to_domain,
+    size_t from_dim,
+    size_t to_dim,
+    utils::offset_t * offsets,
+    size_t num_offsets,
+    utils::id_t * indices,
+    size_t num_indices,
+    bool read) {
+    // TODO - this is an initial implementation for testing purposes.
+    // We may wish to store the buffer pointers coming from Legion directly
+    // into the connectivity
+    auto & conn = topology[from_domain][to_domain].get(from_dim, to_dim);
+
+    auto & id_storage = conn.get_index_space().ids;
+    id_storage = full_array(indices, num_indices, read);
+
+    conn.offsets().storage() = full_array(offsets, num_offsets, read);
+  } // init_connectivities
 
   template<class T, size_t DOM, class... ARG_TYPES>
   T * make(ARG_TYPES &&... args) {
-    using dtype = domain_entity_u<DOM, T>;
-
-    auto & is = index_spaces[DOM][T::dimension].template cast<dtype>();
-    size_t entity = is.size();
-
-    auto placement_ptr = static_cast<T *>(is.storage()->buffer()) + entity;
-    auto ent = new(placement_ptr) T(std::forward<ARG_TYPES>(args)...);
-
-    id_t global_id = id_t::make<T::dimension, DOM>(entity, color);
-    ent->set_global_id(global_id);
-
-    auto & id_storage = is.id_storage();
-
-    id_storage[entity] = global_id;
-
-    is.pushed();
-
-    return ent;
+    auto & is = index_spaces[DOM][T::dimension];
+    const std::size_t i = is.ids.size();
+    const id_t global = id_t::make<T::dimension, DOM>(i, color);
+    is.ids.push_back(global);
+    return make2<T, DOM>(i, global, std::forward<ARG_TYPES>(args)...);
   } // make
 
   template<class T, size_t DOM, class... ARG_TYPES>
   T * make(const id_t & id, ARG_TYPES &&... args) {
-    using dtype = domain_entity_u<DOM, T>;
-
-    auto & is = index_spaces[DOM][T::dimension].template cast<dtype>();
+    auto & is = index_spaces[DOM][T::dimension];
 
     size_t entity = id.entity();
-
-    auto placement_ptr = static_cast<T *>(is.storage()->buffer()) + entity;
-    auto ent = new(placement_ptr) T(std::forward<ARG_TYPES>(args)...);
-
-    ent->set_global_id(id);
-
-    auto & id_storage = is.id_storage();
-
-    id_storage[entity] = id;
-
-    is.pushed();
-
-    return ent;
+    is.ids[entity] = id;
+    return make2<T, DOM>(entity, id, std::forward<ARG_TYPES>(args)...);
   } // make
 
-}; // class hpx_topology_storage_policy_t
+private:
+  template<class T>
+  static utils::vector_ref<T>
+  full_array(T * p, std::size_t n, bool full = true) {
+    return {{p, n}, full ? n : 0};
+  }
+
+  template<class T, size_t DOM, class... ARG_TYPES>
+  T * make2(std::size_t index, const id_t & global, ARG_TYPES &&... args) {
+    const auto ret = new(
+      static_cast<T *>(index_spaces[DOM][T::dimension].data.buffer()) + index)
+      T(std::forward<ARG_TYPES>(args)...);
+    ret->set_global_id(global);
+    return ret;
+  }
+}; // class hpx_topology_storage_policy_u
 
 } // namespace topology
 } // namespace flecsi
-
-#endif // flecsi_topology_hpx_topology_storage_policy_h
-
-/*~-------------------------------------------------------------------------~-*
- * Formatting options for vim.
- * vim: set tabstop=2 shiftwidth=2 expandtab :
- *~-------------------------------------------------------------------------~-*/
