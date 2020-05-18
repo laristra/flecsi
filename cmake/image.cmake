7a88d58bae324d1e849211ca23939e5462feda77
Ben Bergen <bergen@lanl.gov>
2020-05-18 17:16:02 +0000

    Modification for docker (instead of podman)

---
+++ cmake/image.cmake
@@ -0,0 +1,32 @@
+#------------------------------------------------------------------------------#
+#  @@@@@@@@  @@           @@@@@@   @@@@@@@@ @@
+# /@@/////  /@@          @@////@@ @@////// /@@
+# /@@       /@@  @@@@@  @@    // /@@       /@@
+# /@@@@@@@  /@@ @@///@@/@@       /@@@@@@@@@/@@
+# /@@////   /@@/@@@@@@@/@@       ////////@@/@@
+# /@@       /@@/@@//// //@@    @@       /@@/@@
+# /@@       @@@//@@@@@@ //@@@@@@  @@@@@@@@ /@@
+# //       ///  //////   //////  ////////  //
+#
+# Copyright (c) 2016, Triad National Security, LLC
+# All rights reserved
+#------------------------------------------------------------------------------#
+
+function(add_image target tag dockerfile)
+
+  set(options)
+  set(one_value_args)
+  set(multi_value_args ARGS DEPENDS)
+
+  cmake_parse_arguments(image "${options}" "${one_value_args}"
+    "${multi_value_args}" ${ARGN})
+
+  foreach(arg ${image_ARGS})
+    list(APPEND args "--build-arg;${arg}")
+  endforeach()
+
+  add_custom_target(${target}
+    ${ENGINE} build ${args} -t ${tag} -f ${CMAKE_SOURCE_DIR}/${dockerfile} .
+    DEPENDS ${dockerfile} ${image_DEPENDS})
+
+endfunction()
