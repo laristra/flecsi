c9c9e7de9e53be5ba17f27fc63d57c84ebdcaf72
Davis Herring <herring@lanl.gov>
2020-06-26 14:45:43 -0600

    Merge set_intersection test into set_utils

--- flecsi/util/test/set_utils.cc
+++ flecsi/util/test/set_utils.cc
@@ -13,6 +13,7 @@
                                                                               */
 
 #include "flecsi/util/set_utils.hh"
+#include "flecsi/util/set_intersection.hh"
 #include "flecsi/util/unit.hh"
 
 #include <iostream>
@@ -24,6 +25,13 @@ print_set(const char * const prefix, const std::set<T> & set) {
   for(auto i = set.begin(); i != set.end(); ++i)
     UNIT_CAPTURE() << " " << *i;
   UNIT_CAPTURE() << " }" << std::endl;
+}
+
+template<class CONTAINER>
+inline bool
+intersects(const CONTAINER & one, const CONTAINER & two) {
+  return flecsi::util::intersects(
+    one.begin(), one.end(), two.begin(), two.end());
 }
 
 int
@@ -93,6 +101,34 @@ set_utils() {
     print_set("difference(d,d)", set_difference(a, d));
     print_set("difference(d,e)", set_difference(a, e));
     print_set("difference(e,e)", set_difference(a, e));
+
+    {
+      std::vector<int> a = {1, 3, 5, 7, 10, 11};
+      std::vector<int> b = {2, 4, 6, 8, 10, 12};
+      std::vector<int> c = {0, 20};
+      std::vector<int> d = {10, 20, 30};
+      std::vector<int> e = {};
+      std::vector<int> f = {};
+
+      EXPECT_EQ(intersects(a, a), true);
+      EXPECT_EQ(intersects(a, b), true);
+      EXPECT_EQ(intersects(a, c), false);
+      EXPECT_EQ(intersects(a, d), true);
+      EXPECT_EQ(intersects(b, a), true);
+      EXPECT_EQ(intersects(b, b), true);
+      EXPECT_EQ(intersects(b, c), false);
+      EXPECT_EQ(intersects(b, d), true);
+      EXPECT_EQ(intersects(c, a), false);
+      EXPECT_EQ(intersects(c, b), false);
+      EXPECT_EQ(intersects(c, c), true);
+      EXPECT_EQ(intersects(c, d), true);
+      EXPECT_EQ(intersects(d, a), true);
+      EXPECT_EQ(intersects(d, b), true);
+      EXPECT_EQ(intersects(d, c), true);
+      EXPECT_EQ(intersects(d, d), true);
+      EXPECT_EQ(intersects(a, e), false);
+      EXPECT_EQ(intersects(e, f), false);
+    }
 
     // compare
     EXPECT_TRUE(UNIT_EQUAL_BLESSED("set_utils.blessed"));
