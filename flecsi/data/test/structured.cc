2af1622ca8174c00bcf2f3f9721079d137e2a166
Davis Herring <herring@lanl.gov>
2020-06-26 13:24:31 -0600

    Merge privilege test into structured
    It's the only other unconditional test

--- flecsi/data/test/structured.cc
+++ flecsi/data/test/structured.cc
@@ -20,6 +20,25 @@
 
 using namespace flecsi;
 
+template<partition_privilege_t... PP, std::size_t... II>
+constexpr void
+test(std::index_sequence<II...>) {
+  constexpr auto p = privilege_pack<PP...>;
+  static_assert(((get_privilege(II, p) == PP) && ...));
+}
+template<partition_privilege_t... PP>
+constexpr bool
+test() {
+  test<PP...>(std::make_index_sequence<sizeof...(PP)>());
+  return true;
+}
+
+static_assert(test<rw>());
+static_assert(test<wo, rw>());
+static_assert(test<ro, wo, rw>());
+static_assert(test<nu, ro, wo, rw>());
+
+// ---------------
 struct block : topo::specialization<topo::structured, block> {
   static coloring color() {
     coloring c = 10;
