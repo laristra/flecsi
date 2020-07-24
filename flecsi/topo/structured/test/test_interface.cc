7cee8688f2a6cff8e4b84a27af4493534c682cc1
Navamita Ray <nray@lanl.gov>
2020-07-24 12:55:09 -0600

    adding blessed files for testing

--- flecsi/topo/structured/test/test_interface.cc
+++ flecsi/topo/structured/test/test_interface.cc
@@ -37,17 +37,30 @@ test_mesh::slot tmesh;
 test_mesh::cslot coloring;
 
 int
-print() {
+topo_driver() {
   UNIT {
+    // Define bounds of a structured mesh
+    size_t grid_size[1] = {6};
+    size_t nhalo = 1;
+    size_t nhalo_domain = 2;
+    size_t thru_dim = 0;
+    size_t ncolors[1] = {2};
+
+    coloring.allocate(grid_size, nhalo, nhalo_domain, thru_dim, ncolors);
+    tmesh.allocate(coloring.get());
+
     auto colored_ents = coloring.get();
+    int owner = colored_ents.exclusive[0].colors[0];
     int dim = colored_ents.mesh_dim;
-    int edim = colored_ents.entity_dim;
+
+    std::string fname =
+      "smesh_" + std::to_string(dim) + "d_" + std::to_string(owner) + ".txt";
 
     UNIT_CAPTURE() << "MESH DIM " << dim << std::endl;
 
     // Print colored ents: overlay, exclusive, shared, ghost, domain-halos
+    int edim = colored_ents.entity_dim;
     size_t de_nboxes = colored_ents.num_boxes;
-    ;
     auto de_ebox = colored_ents.exclusive;
     auto de_obox = colored_ents.overlay;
     auto de_strides = colored_ents.strides;
@@ -79,30 +92,8 @@ print() {
                        << de_ebox[n].domain.lowerbnd[i] << ", "
                        << de_ebox[n].domain.upperbnd[i] << std::endl;
     }
-  };
-}
-
-int
-topo_driver() {
-  UNIT {
-    // Define bounds of a structured mesh
-    size_t grid_size[1] = {6};
-    size_t nhalo = 1;
-    size_t nhalo_domain = 2;
-    size_t thru_dim = 0;
-    size_t ncolors[1] = {2};
-
-    coloring.allocate(grid_size, nhalo, nhalo_domain, thru_dim, ncolors);
-
-    tmesh.allocate(coloring.get());
-    EXPECT_EQ(test<print>(), 0);
-
-    auto colored_ents = coloring.get();
-    int dim = colored_ents.mesh_dim;
-    int owner = colored_ents.exclusive[0].colors[0];
-    std::string fname =
-      "smesh_" + std::to_string(dim) + "d_" + std::to_string(owner) + ".txt";
     UNIT_WRITE(fname);
+    UNIT_EQUAL_BLESSED(fname.c_str());
   };
 } // ntree_driver
 
