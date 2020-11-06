b86555c46ed42044d55d19ebb992edf1ece4dca5
Davis Herring <herring@lanl.gov>
2020-11-06 16:59:38 -0700

    Separate size.hh out of index.hh
    This makes topo::resize available to accessors

--- flecsi/topo/index.hh
+++ flecsi/topo/index.hh
@@ -20,57 +20,11 @@
 #endif
 
 #include "flecsi/data/accessor.hh"
-#include "flecsi/data/topology.hh"
 #include "flecsi/exec/launch.hh"
-#include "flecsi/topo/core.hh"
+#include "flecsi/topo/size.hh"
 
 namespace flecsi {
 namespace topo {
-
-struct index_base {
-  struct coloring {
-    coloring(size_t size) : size_(size) {}
-
-    size_t size() const {
-      return size_;
-    }
-
-  private:
-    size_t size_;
-  };
-};
-
-// A topology with one index point per color.
-// Suitable for the single layout but not the ragged layout, which is
-// implemented in terms of this topology.
-template<class P>
-struct color_category : index_base, data::partitioned<data::partition> {
-  color_category(const coloring & c)
-    : partitioned(data::make_region<P>({c.size(), 1})) {}
-};
-template<>
-struct detail::base<color_category> {
-  using type = index_base;
-};
-
-// A subtopology for storing/updating row sizes of a partition.
-struct resize : specialization<color_category, resize> {
-  // cslot is useless without a color function, but we don't need it.
-  using Field = flecsi::field<data::partition::row, data::single>;
-  static const Field::definition<resize> field;
-  using accessor = data::accessor_member<field, privilege_pack<ro>>;
-};
-// Now that resize is complete:
-inline const resize::Field::definition<resize> resize::field;
-
-// To control initialization order:
-struct with_size {
-  with_size(std::size_t n) : sz(n) {}
-  auto sizes() {
-    return resize::field(sz);
-  }
-  resize::core sz;
-};
 
 namespace zero {
 inline std::size_t
